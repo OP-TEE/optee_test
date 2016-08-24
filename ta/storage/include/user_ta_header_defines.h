628ff0ba7195191483dac6738658bac063fd997b
Lijianhui (Airbak) <airbak.li@hisilicon.com>
2016-08-24 17:35:55 +0800

    xtest: add 6016 to test concurency storage
    This testcase against to RPMB secure storage.
    Create multiple threads to do secure storage, before the
    modify that move lock to rpmb fs layer, it will fail.
    
    Change-Id: I4138f8f7332a0038378b86ed7dc111cf1c6ad86b
    Signed-off-by: Lijianhui (Airbak) <airbak.li@hisilicon.com>
    [Restricted test to RPMB only, removed begin/end subcase]
    [Removed #ifdef USER_SPACE, create sessions before threads]
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- ta/storage/include/user_ta_header_defines.h
+++ ta/storage/include/user_ta_header_defines.h
@@ -37,7 +37,7 @@
  * as it is used by the ytest
  */
 #define TA_FLAGS		(TA_FLAG_USER_MODE | TA_FLAG_EXEC_DDR | \
-				TA_FLAG_SINGLE_INSTANCE)
+				TA_FLAG_MULTI_SESSION)
 #define TA_STACK_SIZE		(2 * 1024)
 #define TA_DATA_SIZE		(32 * 1024)
 
