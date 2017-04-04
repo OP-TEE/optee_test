OP-TEE/optee_test

commit 69f914daa4dc86f1c1daba5a78faa5df4413d22a

xtest: fail straight if failing to open tee context
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

host/xtest/xtest_main.c
@@ -64,6 +64,7 @@ int main(int argc, char *argv[])
 {
 	int opt;
 	int index;
+	TEEC_Result tee_res;
 	int ret;
 	char *p = (char *)glevel;
 	char *test_suite = (char *)gsuitename;
@@ -115,7 +116,11 @@ int main(int argc, char *argv[])
 
 	printf("\nTEE test application started with device [%s]\n", _device);
 
-	xtest_teec_ctx_init();
+	tee_res = xtest_teec_ctx_init();
+	if (tee_res != TEEC_SUCCESS) {
+		fprintf(stderr, "Failed to open TEE context: 0x%x\n", token);
+		return -1;
+	}
 
 	if (strcmp(test_suite, "regression") == 0)
 		ret = Do_ADBG_RunSuite(&ADBG_Suite_regression,

commit 7d18f5550707da4323e859f4e56ac95f7609f7a2

xtest: fix bug
Fixes: 69f914daa4dc ("xtest: fail straight if failing to open tee context")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

host/xtest/xtest_main.c
@@ -13,6 +13,7 @@
  */
 
 #include <err.h>
+#include <inttypes.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -118,7 +119,8 @@ int main(int argc, char *argv[])
 
 	tee_res = xtest_teec_ctx_init();
 	if (tee_res != TEEC_SUCCESS) {
-		fprintf(stderr, "Failed to open TEE context: 0x%x\n", token);
+		fprintf(stderr, "Failed to open TEE context: 0x%" PRIx32 "\n",
+								tee_res);
 		return -1;
 	}
 