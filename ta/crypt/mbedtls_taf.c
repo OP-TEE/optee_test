f7ffa64196293d3e7741c003a53cd661caf5f24e
Jens Wiklander <jens.wiklander@linaro.org>
2018-04-20 16:25:21 +0200

    ta/crypt: add TA_CRYPT_CMD_MBED_CHECK_CERT
    Adds the TAF TA_CRYPT_CMD_MBED_CHECK_CERT which uses mbedTLS to check a
    certificate chain.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- ta/crypt/mbedtls_taf.c
+++ ta/crypt/mbedtls_taf.c
@@ -11,6 +11,8 @@
 #include <mbedtls/sha1.h>
 #include <mbedtls/sha256.h>
 #include <mbedtls/x509.h>
+#include <mbedtls/x509_crt.h>
+
 
 TEE_Result
 ta_entry_mbedtls_self_tests(uint32_t param_type,
@@ -45,4 +47,54 @@ ta_entry_mbedtls_self_tests(uint32_t param_type,
 #else
 	return TEE_ERROR_NOT_IMPLEMENTED;
 #endif
+}
+
+TEE_Result ta_entry_mbedtls_check_cert(uint32_t param_type,
+				    TEE_Param params[TEE_NUM_PARAMS])
+{
+	TEE_Result res = TEE_SUCCESS;
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
+						TEE_PARAM_TYPE_MEMREF_INPUT,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE);
+	int ret;
+	uint32_t flags;
+	mbedtls_x509_crt crt;
+	mbedtls_x509_crt trust_crt;
+
+	if (param_type != exp_pt)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	mbedtls_x509_crt_init(&crt);
+	mbedtls_x509_crt_init(&trust_crt);
+
+	ret = mbedtls_x509_crt_parse(&crt, params[0].memref.buffer,
+				     params[0].memref.size);
+	if (ret) {
+		EMSG("mbedtls_x509_crt_parse: failed: %#x", ret);
+		return TEE_ERROR_BAD_FORMAT;
+	}
+
+	ret = mbedtls_x509_crt_parse(&trust_crt, params[1].memref.buffer,
+				     params[1].memref.size);
+	if (ret) {
+		EMSG("mbedtls_x509_crt_parse: failed: %#x", ret);
+		res = TEE_ERROR_BAD_FORMAT;
+		goto out;
+	}
+
+	ret = mbedtls_x509_crt_verify(&crt, &trust_crt, NULL, NULL, &flags,
+				      NULL, NULL);
+	if (ret) {
+		EMSG("mbedtls_x509_crt_verify: failed: %#x", ret);
+		res = TEE_ERROR_BAD_FORMAT;
+
+	}
+
+out:
+	mbedtls_x509_crt_free(&trust_crt);
+	mbedtls_x509_crt_free(&crt);
+
+	return res;
+
 }
