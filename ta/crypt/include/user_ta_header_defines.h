578c832189629851dcadbddc0e059bef0bc5ea71
Jens Wiklander <jens.wiklander@linaro.org>
2018-04-20 11:20:01 +0200

    ta/crypt: increase stack size
    Increases stack size due to anticipated stack usage by mbedTLS.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- ta/crypt/include/user_ta_header_defines.h
+++ ta/crypt/include/user_ta_header_defines.h
@@ -33,7 +33,7 @@
 #define TA_UUID TA_CRYPT_UUID
 
 #define TA_FLAGS		(TA_FLAG_USER_MODE | TA_FLAG_EXEC_DDR)
-#define TA_STACK_SIZE		(2 * 1024)
+#define TA_STACK_SIZE		(32 * 1024)
 #define TA_DATA_SIZE		(32 * 1024)
 
 #endif /* USER_TA_HEADER_DEFINES_H */
