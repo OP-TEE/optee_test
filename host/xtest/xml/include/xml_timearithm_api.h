OP-TEE/optee_test

commit e4ec9e4aca080e361fa48d713d3b1cf05f85ad20

xtest: Explicitly initialize local variables
This change initializes all local variables to prevent build issues
(warnings and/or errors) in OP-TEE test package. This change uses
= { } for non-scalar variables and = 0 for scalar variables when there
is no obvious value. This change falls back to memset() only when
neither = { } nor = 0 is applicable, here pthread_t typed variable
and variable size arrays.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

host/xtest/xml/include/xml_timearithm_api.h
@@ -556,11 +556,9 @@ static TEEC_Result Invoke_BigIntShiftRight(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM02->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM02->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
-
-	memset(tmp1, 0, SHARE_MEM02->size);
 
 	if (!BN_bin2bn(SHARE_MEM01->buffer, SHARE_MEM01->size, a))
 		goto exit;
@@ -653,11 +651,9 @@ static TEEC_Result Invoke_BigIntDiv_Remain(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
-
-	memset(tmp1, 0, SHARE_MEM03->size);
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
 		       a))
@@ -735,11 +731,9 @@ static TEEC_Result Invoke_BigIntDiv_Quotient(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)caalloc(SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
-
-	memset(tmp1, 0, SHARE_MEM03->size);
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
 		       a))
@@ -817,11 +811,9 @@ static TEEC_Result Invoke_BigIntAdd(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)caalloc(SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
-
-	memset(tmp1, 0, SHARE_MEM03->size);
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
 		       a))
@@ -985,11 +977,9 @@ static TEEC_Result Invoke_BigIntAddMod(
 	if (res != TEE_SUCCESS)
 		goto exit;
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
-
-	memset(tmp1, 0, SHARE_MEM03->size);
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
 		       a))
@@ -1070,11 +1060,9 @@ static TEEC_Result Invoke_BigIntSubMod(
 	if (res != TEE_SUCCESS)
 		goto exit;
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
-
-	memset(tmp1, 0, SHARE_MEM03->size);
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
 		       a))
@@ -1192,11 +1180,9 @@ static TEEC_Result Invoke_BigIntMulMod(
 	if (res != TEE_SUCCESS)
 		goto exit;
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
-
-	memset(tmp1, 0, SHARE_MEM03->size);
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
 		       a))
@@ -1368,11 +1354,9 @@ static TEEC_Result Invoke_BigIntSub(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
-
-	memset(tmp1, 0, SHARE_MEM03->size);
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
 		       a))
@@ -1440,17 +1424,13 @@ static TEEC_Result Invoke_BigIntNeg(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
 
-	memset(tmp1, 0, SHARE_MEM03->size);
-
-	tmp2 = (uint8_t *)malloc(SHARE_MEM01->size);
+	tmp2 = (uint8_t *)calloc(1, SHARE_MEM01->size);
 	if (tmp2 == NULL)
 		goto tmp2_exit;
-	memset(tmp2, 0, SHARE_MEM01->size);
-
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
 		       a))
@@ -1607,11 +1587,9 @@ static TEEC_Result Invoke_BigIntMul(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
-
-	memset(tmp1, 0, SHARE_MEM03->size);
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
 		       a))
@@ -1688,17 +1666,14 @@ static TEEC_Result Invoke_BigIntInvMod(
 	if (res != TEE_SUCCESS)
 		goto exit;
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
 
-	memset(tmp1, 0, SHARE_MEM03->size);
-
-	tmp2 = (uint8_t *)malloc(SHARE_MEM01->size);
+	tmp2 = (uint8_t *)calloc(1, SHARE_MEM01->size);
 	if (tmp2 == NULL)
 		goto tmp2_exit;
 
-	memset(tmp2, 0, SHARE_MEM01->size);
 	tmp2[0] = 1;
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
@@ -1775,11 +1750,9 @@ static TEEC_Result Invoke_BigIntSquare(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)malloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
-
-	memset(tmp1, 0, SHARE_MEM03->size);
 
 	if (!BN_bin2bn((uint8_t *)(SHARE_MEM01->buffer), SHARE_MEM01->size,
 		       a))

host/xtest/adbg/src/adbg_case.c
@@ -47,7 +47,7 @@ static const char *ADBG_Case_GetTestID(ADBG_Case_t *Case_p);
 *************************************************************************/
 ADBG_Case_t *ADBG_Case_New(const struct adbg_case_def *case_def)
 {
-	ADBG_Case_t *Case_p;
+	ADBG_Case_t *Case_p = NULL;
 
 	Case_p = calloc(1, sizeof(*Case_p));
 	if (Case_p)
@@ -160,7 +160,7 @@ void Do_ADBG_BeginSubCase(
 	}
 
 	va_list ArgList;
-	char Title[80];
+	char Title[80] = { };
 
 	va_start(ArgList, FormatTitle_p);
 	vsnprintf(Title, sizeof(Title), FormatTitle_p, ArgList);
@@ -189,7 +189,7 @@ void Do_ADBG_EndSubCase(
 	)
 {
 	va_list ArgList;
-	char Title[80];
+	char Title[80] = { };
 	ADBG_SubCase_t *SubCase_p = NULL;
 
 	if (Case_p == NULL) {
@@ -277,7 +277,7 @@ static ADBG_SubCase_t *ADBG_Case_CreateSubCase(
 	const char *const Title_p
 	)
 {
-	ADBG_SubCase_t *SubCase_p;
+	ADBG_SubCase_t *SubCase_p = NULL;
 
 	SubCase_p = calloc(1, sizeof(*SubCase_p));
 	if (SubCase_p == NULL)
@@ -301,7 +301,7 @@ static ADBG_SubCase_t *ADBG_Case_CreateSubCase(
 		Case_p->FirstSubCase_p = SubCase_p;
 	} else {
 		ADBG_SubCase_t *Parent_p = SubCase_p->Parent_p;
-		char PrefixTitle[80];
+		char PrefixTitle[80] = { };
 
 		/* Update parent SubCase */
 		Parent_p->Result.NumSubCases++;

host/xtest/adbg/src/adbg_log.c
@@ -59,7 +59,7 @@ void Do_ADBG_HexLog(
 	)
 {
 	const uint8_t *Data_p = Buf_p;
-	size_t n;
+	size_t n = 0;
 
 	for (n = 0; n < Size; n += Cols) {
 		char HexBuffer[ADBG_STRING_LENGTH_MAX];

host/xtest/adbg/src/adbg_run.c
@@ -60,7 +60,7 @@ int Do_ADBG_RunSuite(
 	char *argv[]
 	)
 {
-	ADBG_Runner_t *Runner_p;
+	ADBG_Runner_t *Runner_p = NULL;
 
 	Runner_p = calloc(1, sizeof(*Runner_p));
 	if (Runner_p == NULL) {
@@ -81,8 +81,8 @@ int Do_ADBG_AppendToSuite(
 	ADBG_Suite_Definition_t *Source_p
 	)
 {
-	char *p;
-	size_t size;
+	char *p = NULL;
+	size_t size = 0;
 
 	/* Append name of 'Source_p' to name of 'Dest_p' */
 	size = strlen(Source_p->SuiteID_p);
@@ -117,10 +117,10 @@ static int ADBG_RunSuite(
 	char *argv[]
 	)
 {
-	ADBG_Case_t *Case_p;
+	ADBG_Case_t *Case_p = NULL;
 	size_t NumSkippedTestCases = 0;
 	int failed_test = 0;
-	struct adbg_case_def *case_def;
+	struct adbg_case_def *case_def = NULL;
 
 	Do_ADBG_Log("######################################################");
 	Do_ADBG_Log("#");
@@ -131,7 +131,7 @@ static int ADBG_RunSuite(
 	TAILQ_FOREACH(case_def, &Runner_p->Suite_p->cases, link) {
 		if (argc > 0) {
 			bool HaveMatch = false;
-			int i;
+			int i = 0;
 
 			for (i = 0; i < argc; i++) {
 
@@ -191,7 +191,7 @@ static int ADBG_RunSuite(
 
 	Do_ADBG_Log("+-----------------------------------------------------");
 	if (argc > 0) {
-		int i;
+		int i = 0;
 
 		for (i = 0; i < argc; i++)
 			Do_ADBG_Log(

host/xtest/adbg/src/security_utils_hex.c
@@ -152,12 +152,12 @@ size_t SecUtil_BufferToHex(
 	)
 {
 	const uint8_t *Data_p = Buffer_p;
-	size_t UsedDestLength;
-	size_t n;
+	size_t UsedDestLength = 0;
+	size_t n = 0;
 	const char *ByteSeparator_p = NULL;
 	const char *GroupSeparator_p = NULL;
 	const char *BytePrefix_p = NULL;
-	size_t BytePrefixLength;
+	size_t BytePrefixLength = 0;
 
 	if (DestinationLength > 1)
 		Destination_p[0] = '\0';
@@ -186,8 +186,8 @@ size_t SecUtil_BufferToHex(
 	 * the absense of both snprintf and strlcat
 	 */
 	for (n = 0; n < BufferLength; n++) {
-		const char *Separator_p;
-		size_t SeparatorLength;
+		const char *Separator_p = NULL;
+		size_t SeparatorLength = 0;
 
 		/* Establish separator for this byte and the next */
 		if (n == BufferLength - 1)
@@ -201,7 +201,7 @@ size_t SecUtil_BufferToHex(
 
 		/* Insert the Byte prefix */
 		if (UsedDestLength < DestinationLength) {
-			size_t CopyLength;
+			size_t CopyLength = 0;
 
 			CopyLength = MIN(BytePrefixLength,
 					 DestinationLength - UsedDestLength);
@@ -229,7 +229,7 @@ size_t SecUtil_BufferToHex(
 
 		/* Insert the separator */
 		if (UsedDestLength < DestinationLength) {
-			size_t CopyLength;
+			size_t CopyLength = 0;
 
 			CopyLength = MIN(SeparatorLength,
 					 DestinationLength - UsedDestLength);

host/xtest/aes_perf.c
@@ -43,6 +43,7 @@
 #include <unistd.h>
 
 #include "crypto_common.h"
+#include "xtest_helpers.h"
 
 #ifdef CFG_SECURE_DATA_PATH
 #include "sdp_basic.h"
@@ -129,9 +130,9 @@ static void check_res(TEEC_Result res, const char *errmsg, uint32_t *orig)
 
 static void open_ta(void)
 {
-	TEEC_Result res;
+	TEEC_Result res = TEEC_ERROR_GENERIC;
 	TEEC_UUID uuid = TA_AES_PERF_UUID;
-	uint32_t err_origin;
+	uint32_t err_origin = 0;
 
 	res = TEEC_InitializeContext(NULL, &ctx);
 	check_res(res, "TEEC_InitializeContext", NULL);
@@ -259,7 +260,7 @@ static void register_shm(TEEC_SharedMemory *shm, int fd)
 
 static void allocate_shm(TEEC_SharedMemory *shm, size_t sz)
 {
-	TEEC_Result res;
+	TEEC_Result res = TEEC_ERROR_GENERIC;
 
 	shm->buffer = NULL;
 	shm->size = sz;
@@ -330,7 +331,7 @@ static void free_shm(int in_place)
 static ssize_t read_random(void *in, size_t rsize)
 {
 	static int rnd;
-	ssize_t s;
+	ssize_t s = 0;
 
 	if (!rnd) {
 		rnd = open("/dev/urandom", O_RDONLY);
@@ -371,12 +372,11 @@ static uint64_t timespec_diff_ns(struct timespec *start, struct timespec *end)
 
 static void prepare_key(int decrypt, int keysize, int mode)
 {
-	TEEC_Result res;
-	uint32_t ret_origin;
-	TEEC_Operation op;
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	uint32_t ret_origin = 0;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 	uint32_t cmd = TA_AES_PERF_CMD_PREPARE_KEY;
 
-	memset(&op, 0, sizeof(op));
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_VALUE_INPUT,
 					 TEEC_NONE, TEEC_NONE);
 	op.params[0].value.a = decrypt;
@@ -389,8 +389,9 @@ static void prepare_key(int decrypt, int keysize, int mode)
 
 static void do_warmup(int warmup)
 {
-	struct timespec t0, t;
-	int i;
+	struct timespec t0 = { };
+	struct timespec t = { };
+	int i = 0;
 
 	get_current_time(&t0);
 	do {
@@ -447,11 +448,11 @@ void aes_perf_run_test(int mode, int keysize, int decrypt, size_t size, size_t u
 				unsigned int n, unsigned int l, int input_data_init,
 				int in_place, int warmup, int verbosity)
 {
-	struct statistics stats;
-	struct timespec ts;
-	TEEC_Operation op;
+	struct statistics stats = { };
+	struct timespec ts = { };
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 	int n0 = n;
-	double sd;
+	double sd = 0;
 	uint32_t cmd = is_sdp_test ? TA_AES_PERF_CMD_PROCESS_SDP :
 				     TA_AES_PERF_CMD_PROCESS;
 
@@ -478,13 +479,10 @@ void aes_perf_run_test(int mode, int keysize, int decrypt, size_t size, size_t u
 	open_ta();
 	prepare_key(decrypt, keysize, mode);
 
-	memset(&stats, 0, sizeof(stats));
-
 	alloc_buffers(size, in_place, verbosity);
 	if (input_data_init == CRYPTO_USE_ZEROS)
 		run_feed_input(in_shm.buffer, size, 0);
 
-	memset(&op, 0, sizeof(op));
 	/* Using INOUT to handle the case in_place == 1 */
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_PARTIAL_INOUT,
 					 TEEC_MEMREF_PARTIAL_INOUT,
@@ -507,9 +505,10 @@ void aes_perf_run_test(int mode, int keysize, int decrypt, size_t size, size_t u
 		do_warmup(warmup);
 
 	while (n-- > 0) {
-		TEEC_Result res;
-		uint32_t ret_origin;
-		struct timespec t0, t1;
+		TEEC_Result res = TEEC_ERROR_GENERIC;
+		uint32_t ret_origin = 0;
+		struct timespec t0 = { };
+		struct timespec t1 = { };
 
 		if (input_data_init == CRYPTO_USE_RANDOM)
 			run_feed_input(in_shm.buffer, size, 1);
@@ -565,12 +564,10 @@ void aes_perf_run_test(int mode, int keysize, int decrypt, size_t size, size_t u
 
 int aes_perf_runner_cmd_parser(int argc, char *argv[])
 {
-	int i;
-
+	int i = 0;
 	/*
 	* Command line parameters
 	*/
-
 	size_t size = 1024;	/* Buffer size (-s) */
 	size_t unit = CRYPTO_DEF_UNIT_SIZE; /* Divide buffer (-u) */
 	unsigned int n = CRYPTO_DEF_COUNT; /*Number of measurements (-n)*/

host/xtest/benchmark_1000.c
@@ -47,9 +47,9 @@ static TEEC_Result run_test_with_args(enum storage_benchmark_cmd cmd,
 		uint32_t arg3, uint32_t *out0, uint32_t *out1)
 {
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
-	TEEC_Result res;
-	TEEC_Session sess;
-	uint32_t orig;
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	TEEC_Session sess = { };
+	uint32_t orig = 0;
 
 	res = xtest_teec_open_session(&sess, &storage_benchmark_ta_uuid, NULL, &orig);
 	if (res != TEEC_SUCCESS)
@@ -84,7 +84,7 @@ struct test_record {
 static TEEC_Result run_chunk_access_test(enum storage_benchmark_cmd cmd,
 		uint32_t data_size, uint32_t chunk_size, struct test_record *rec)
 {
-	TEE_Result res;
+	TEE_Result res = TEEC_ERROR_GENERIC;
 	uint32_t spent_time = 0;
 
 	res = run_test_with_args(cmd, data_size, chunk_size, DO_VERIFY, 0,
@@ -99,7 +99,7 @@ static TEEC_Result run_chunk_access_test(enum storage_benchmark_cmd cmd,
 
 static void show_test_result(struct test_record records[], size_t size)
 {
-	size_t i;
+	size_t i = 0;
 
 	printf("-----------------+---------------+----------------\n");
 	printf(" Data Size (B) \t | Time (s)\t | Speed (kB/s)\t \n");
@@ -119,7 +119,7 @@ static void chunk_test(ADBG_Case_t *c, enum storage_benchmark_cmd cmd)
 {
 	uint32_t chunk_size = DEFAULT_CHUNK_SIZE;
 	struct test_record records[ARRAY_SIZE(data_size_table) - 1];
-	size_t i;
+	size_t i = 0;
 
 	for (i = 0; data_size_table[i]; i++) {
 		ADBG_EXPECT_TEEC_SUCCESS(c,

host/xtest/install_ta.c
@@ -20,15 +20,17 @@
 #include <sys/types.h>
 #include <tee_client_api.h>
 #include <unistd.h>
-#include "xtest_test.h"
+
 #include "install_ta.h"
+#include "xtest_helpers.h"
+#include "xtest_test.h"
 
 static void *read_ta(const char *dname, const char *fname, size_t *size)
 {
 	char nbuf[PATH_MAX];
-	FILE *f;
-	void *buf;
-	size_t s;
+	FILE *f = NULL;
+	void *buf = NULL;
+	size_t s = 0;
 
 	if (dname)
 		snprintf(nbuf, sizeof(nbuf), "%s/%s", dname, fname);
@@ -58,11 +60,10 @@ static void *read_ta(const char *dname, const char *fname, size_t *size)
 
 static void install_ta(TEEC_Session *sess, void *buf, size_t blen)
 {
-	TEEC_Result res;
-	uint32_t err_origin;
-	TEEC_Operation op;
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	uint32_t err_origin = 0;
+	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 
-	memset(&op, 0, sizeof(op));
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
 	op.params[0].tmpref.buffer = buf;
@@ -78,8 +79,8 @@ static void install_ta(TEEC_Session *sess, void *buf, size_t blen)
 static void install_file(TEEC_Session *sess, const char *dirname,
 			 const char *filename)
 {
-	void *ta;
-	size_t ta_size;
+	void *ta = NULL;
+	size_t ta_size = 0;
 
 	printf("Installing \"%s\"\n", filename);
 	ta = read_ta(dirname, filename, &ta_size);
@@ -89,7 +90,7 @@ static void install_file(TEEC_Session *sess, const char *dirname,
 
 static void install_dir(TEEC_Session *sess, const char *dirname)
 {
-	DIR *dirp;
+	DIR *dirp = NULL;
 
 	printf("Searching directory \"%s\" for TAs\n", dirname);
 	dirp = opendir(dirname);
@@ -113,12 +114,12 @@ static void install_dir(TEEC_Session *sess, const char *dirname)
 
 int install_ta_runner_cmd_parser(int argc, char *argv[])
 {
-	TEEC_Result res;
-	uint32_t err_origin;
+	TEEC_Result res = TEEC_ERROR_GENERIC;
+	uint32_t err_origin = 0;
 	TEEC_UUID uuid = PTA_SECSTOR_TA_MGMT_UUID;
-	TEEC_Context ctx;
-	TEEC_Session sess;
-	int i;
+	TEEC_Context ctx = { };
+	TEEC_Session sess = { };
+	int i = 0;
 
 	res = TEEC_InitializeContext(NULL, &ctx);
 	if (res)
@@ -131,7 +132,7 @@ int install_ta_runner_cmd_parser(int argc, char *argv[])
 			res, err_origin);
 
 	for (i = 1; i < argc; i++) {
-		struct stat sb;
+		struct stat sb = { };
 
 		if (stat(argv[i], &sb)) {
 			printf("Skipping \"%s\": %s", argv[i], strerror(errno));

commit 20b1852bda0921c085a3133ad990a394e8f4e37f

GP: fix compile errors with calloc()
Fixes a few errors when xtest is compiled with GP tests enabled
(CFG_GP_PACKAGE_PATH pointing to the GP Initial Configuration Test
Suite files).

Fixes: e4ec9e4aca08 ("xtest: Explicitly initialize local variables")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

host/xtest/xml/include/xml_timearithm_api.h
@@ -731,7 +731,7 @@ static TEEC_Result Invoke_BigIntDiv_Quotient(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)caalloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
 
@@ -811,7 +811,7 @@ static TEEC_Result Invoke_BigIntAdd(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)caalloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
 
@@ -977,7 +977,7 @@ static TEEC_Result Invoke_BigIntAddMod(
 	if (res != TEE_SUCCESS)
 		goto exit;
 
-	tmp1 = (uint8_t *)calloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
 
@@ -1060,7 +1060,7 @@ static TEEC_Result Invoke_BigIntSubMod(
 	if (res != TEE_SUCCESS)
 		goto exit;
 
-	tmp1 = (uint8_t *)calloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
 
@@ -1180,7 +1180,7 @@ static TEEC_Result Invoke_BigIntMulMod(
 	if (res != TEE_SUCCESS)
 		goto exit;
 
-	tmp1 = (uint8_t *)calloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
 
@@ -1587,7 +1587,7 @@ static TEEC_Result Invoke_BigIntMul(
 
 	res = TEEC_InvokeCommand(sess, cmdId, &op, &org);
 
-	tmp1 = (uint8_t *)calloc(SHARE_MEM03->size);
+	tmp1 = (uint8_t *)calloc(1, SHARE_MEM03->size);
 	if (tmp1 == NULL)
 		goto tmp1_exit;
 