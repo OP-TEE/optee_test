3ee61b6f94b320e27f7ed808360c7ce3eb9b7f53
Etienne Carriere <etienne.carriere@linaro.org>
2019-05-17 08:53:12 +0200

    regression 1021: test panic during session opening
    Use invocation parameters passed with open session request to ask the
    TA to panic during its open session sequence.
    
    This change modifies TA_InvokeCommandEntryPoint() in ta_enry.c to
    get consistent argument labels across the source file.
    
    Signed-off-by: Ovidiu Mihalachi <ovidiu_mihalachi@mentor.com>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

--- ta/sims/include/ta_sims.h
+++ ta/sims/include/ta_sims.h
@@ -31,15 +31,17 @@
 #include <ta_sims_test.h>
 #include <tee_api.h>
 
-TEE_Result sims_open_session(void **ctx);
 TEE_Result sims_open_ta_session(void *ctx, uint32_t nParamTypes,
 				TEE_Param pParams[4]);
-void sims_close_session(void *ctx);
 TEE_Result sims_read(uint32_t nParamTypes, TEE_Param pParams[4]);
 TEE_Result sims_write(uint32_t nParamTypes, TEE_Param pParams[4]);
 TEE_Result sims_get_counter(void *session_context, uint32_t param_types,
 			    TEE_Param params[4]);
 TEE_Result sims_entry_panic(void *session_context, uint32_t nParamTypes,
 			    TEE_Param pParams[4]);
+TEE_Result sims_entry_open_session(void **session,
+				   uint32_t param_types, TEE_Param params[4]);
+
+void sims_entry_close_session(void *session);
 
 #endif
--- ta/sims/ta_entry.c
+++ ta/sims/ta_entry.c
@@ -45,41 +45,39 @@ void TA_DestroyEntryPoint(void)
 }
 
 /* Called each time a session is opened */
-TEE_Result TA_OpenSessionEntryPoint(uint32_t nParamTypes, TEE_Param pParams[4],
-				    void **ppSessionContext)
+TEE_Result TA_OpenSessionEntryPoint(uint32_t param_types, TEE_Param params[4],
+				    void **session_context)
 {
-	(void)nParamTypes;
-	(void)pParams;
-	return sims_open_session(ppSessionContext);
+	return sims_entry_open_session(session_context, param_types, params);
 }
 
 /* Called each time a session is closed */
-void TA_CloseSessionEntryPoint(void *pSessionContext)
+void TA_CloseSessionEntryPoint(void *session_context)
 {
-	sims_close_session(pSessionContext);
+	sims_entry_close_session(session_context);
 }
 
 /* Called when a command is invoked */
-TEE_Result TA_InvokeCommandEntryPoint(void *pSessionContext,
-				      uint32_t nCommandID, uint32_t nParamTypes,
-				      TEE_Param pParams[4])
+TEE_Result TA_InvokeCommandEntryPoint(void *session_context,
+				      uint32_t command_id, uint32_t param_types,
+				      TEE_Param params[4])
 {
-	switch (nCommandID) {
+	switch (command_id) {
 	case TA_SIMS_OPEN_TA_SESSION:
-		return sims_open_ta_session(pSessionContext,
-					    nParamTypes, pParams);
+		return sims_open_ta_session(session_context,
+					    param_types, params);
 
 	case TA_SIMS_CMD_READ:
-		return sims_read(nParamTypes, pParams);
+		return sims_read(param_types, params);
 
 	case TA_SIMS_CMD_WRITE:
-		return sims_write(nParamTypes, pParams);
+		return sims_write(param_types, params);
 
 	case TA_SIMS_CMD_GET_COUNTER:
-		return sims_get_counter(pSessionContext, nParamTypes, pParams);
+		return sims_get_counter(session_context, param_types, params);
 
 	case TA_SIMS_CMD_PANIC:
-		return sims_entry_panic(pSessionContext, nParamTypes, pParams);
+		return sims_entry_panic(session_context, param_types, params);
 
 	default:
 		return TEE_ERROR_BAD_PARAMETERS;
