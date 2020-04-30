e8a3e8457737a069741b8fd78ffc5eec328e8388
Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>
2020-04-30 16:59:35 +0300

    xtest: add UUIDv5 calculation helper
    UUIDv5 is specific in RFC 4122.
    
    This implements section (for SHA-1):
    4.3.  Algorithm for Creating a Name-Based UUID
    
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Vesa Jääskeläinen <vesa.jaaskelainen@vaisala.com>

--- host/xtest/xtest_uuid_helpers.c
+++ host/xtest/xtest_uuid_helpers.c
@@ -4,6 +4,8 @@
  */
 
 #include <ctype.h>
+#include <endian.h>
+#include <openssl/evp.h>
 #include <stdint.h>
 #include <string.h>
 #include <tee_api_types.h>
@@ -75,5 +77,73 @@ TEEC_Result xtest_uuid_from_str(TEEC_UUID *uuid, const char *s)
 	}
 	*uuid = u;
 out:
+	return res;
+}
+
+TEEC_Result xtest_uuid_v5(TEEC_UUID *uuid, const TEEC_UUID *ns,
+			  const void *name, size_t size)
+{
+	TEEC_Result res = TEEC_SUCCESS;
+	EVP_MD_CTX *mdctx = NULL;
+	const EVP_MD *md = NULL;
+	unsigned char hash[EVP_MAX_MD_SIZE] = { };
+	unsigned int md_len = 0;
+	unsigned char nsbe[16] = { };
+	uint32_t be32 = 0;
+	uint16_t be16 = 0;
+	int ret = 0;
+
+	/* Convert from host to big endian */
+	be32 = htobe32(ns->timeLow);
+	memcpy(&nsbe[0], &be32, sizeof(be32));
+	be16 = htobe16(ns->timeMid);
+	memcpy(&nsbe[4], &be16, sizeof(be16));
+	be16 = htobe16(ns->timeHiAndVersion);
+	memcpy(&nsbe[6], &be16, sizeof(be16));
+	memcpy(&nsbe[8], &ns->clockSeqAndNode, sizeof(ns->clockSeqAndNode));
+
+	mdctx = EVP_MD_CTX_create();
+	if (!mdctx)
+		return TEEC_ERROR_OUT_OF_MEMORY;
+	md = EVP_sha1();
+	if (!md) {
+		res = TEEC_ERROR_NOT_SUPPORTED;
+		goto out;
+	}
+	ret = EVP_DigestInit_ex(mdctx, md, NULL);
+	if (!ret) {
+		res = TEEC_ERROR_GENERIC;
+		goto out;
+	}
+	ret = EVP_DigestUpdate(mdctx, nsbe, sizeof(nsbe));
+	if (!ret) {
+		res = TEEC_ERROR_GENERIC;
+		goto out;
+	}
+	ret = EVP_DigestUpdate(mdctx, name, size);
+	if (!ret) {
+		res = TEEC_ERROR_GENERIC;
+		goto out;
+	}
+	ret = EVP_DigestFinal_ex(mdctx, hash, &md_len);
+	if (!ret) {
+		res = TEEC_ERROR_GENERIC;
+		goto out;
+	}
+
+	/* Mark it as UUIDv5 */
+	hash[6] = (hash[6] & 0x0F) | 0x50;
+	hash[8] = (hash[8] & 0x3F) | 0x80;
+
+	/* Convert from big endian to host */
+	memcpy(&be32, &hash[0], sizeof(uint32_t));
+	uuid->timeLow = be32toh(be32);
+	memcpy(&be16, &hash[4], sizeof(uint16_t));
+	uuid->timeMid = be16toh(be16);
+	memcpy(&be16, &hash[6], sizeof(uint16_t));
+	uuid->timeHiAndVersion = be16toh(be16);
+	memcpy(uuid->clockSeqAndNode, &hash[8], sizeof(uuid->clockSeqAndNode));
+out:
+	EVP_MD_CTX_destroy(mdctx);
 	return res;
 }
