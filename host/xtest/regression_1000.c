OP-TEE/optee_test

commit 8e070bc41e1d5494a8716288454ac2b82d70302a

xtest: disable tests 1027 and 1028 if no OpenSSL
Tests 1027 and 1028 depend on xtest_uuid_v5() which in turn depends on OpenSSL
APIs so they should just return if OPENSSL_FOUND is not defined.

Signed-off-by: Victor Chong <victor.chong@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome@forissier.org>

host/xtest/regression_1000.c
@@ -2085,6 +2085,7 @@ ADBG_CASE_DEFINE(regression, 1026, xtest_tee_test_1026,
 
 static void xtest_tee_test_1027(ADBG_Case_t *c)
 {
+#ifdef OPENSSL_FOUND
 	TEEC_Result result = TEEC_ERROR_GENERIC;
 	uint32_t ret_orig = 0;
 	TEEC_Session session = { };
@@ -2125,6 +2126,11 @@ static void xtest_tee_test_1027(ADBG_Case_t *c)
 
 out:
 	TEEC_CloseSession(&session);
+#else /*!OPENSSL_FOUND*/
+	UNUSED(c);
+	/* xtest_uuid_v5() depends on OpenSSL */
+	Do_ADBG_Log("OpenSSL not available, skipping test 1027");
+#endif
 }
 
 ADBG_CASE_DEFINE(regression, 1027, xtest_tee_test_1027,
@@ -2132,6 +2138,7 @@ ADBG_CASE_DEFINE(regression, 1027, xtest_tee_test_1027,
 
 static void xtest_tee_test_1028(ADBG_Case_t *c)
 {
+#ifdef OPENSSL_FOUND
 	TEEC_Result result = TEEC_ERROR_GENERIC;
 	uint32_t ret_orig = 0;
 	TEEC_Session session = { };
@@ -2175,6 +2182,11 @@ static void xtest_tee_test_1028(ADBG_Case_t *c)
 
 out:
 	TEEC_CloseSession(&session);
+#else /*!OPENSSL_FOUND*/
+	UNUSED(c);
+	/* xtest_uuid_v5() depends on OpenSSL */
+	Do_ADBG_Log("OpenSSL not available, skipping test 1028");
+#endif
 }
 
 ADBG_CASE_DEFINE(regression, 1028, xtest_tee_test_1028,

host/xtest/xtest_uuid_helpers.c
@@ -5,7 +5,9 @@
 
 #include <ctype.h>
 #include <endian.h>
+#ifdef OPENSSL_FOUND
 #include <openssl/evp.h>
+#endif
 #include <stdint.h>
 #include <string.h>
 #include <tee_api_types.h>
@@ -80,6 +82,7 @@ out:
 	return res;
 }
 
+#ifdef OPENSSL_FOUND
 TEEC_Result xtest_uuid_v5(TEEC_UUID *uuid, const TEEC_UUID *ns,
 			  const void *name, size_t size)
 {
@@ -147,3 +150,4 @@ out:
 	EVP_MD_CTX_destroy(mdctx);
 	return res;
 }
+#endif /*OPENSSL_FOUND*/

host/xtest/xtest_uuid_helpers.h
@@ -15,9 +15,11 @@
  */
 TEEC_Result xtest_uuid_from_str(TEEC_UUID *uuid, const char *s);
 
+#ifdef OPENSSL_FOUND
 /*
  * Form UUIDv5 from given name space and name.
  */
 TEEC_Result xtest_uuid_v5(TEEC_UUID *uuid, const TEEC_UUID *ns,
 			  const void *name, size_t size);
+#endif /*OPENSSL_FOUND*/
 #endif

commit 136007d4749a4336cdeeb3d5a879d8d511497913

xtest: regression 1027: fix build warning when openssl not used
Fix build warning reported with trace message like below:

.../optee-test-custom/host/xtest/regression_1000.c:2049:20: error: ‘client_uuid_linux_ns’ defined but not used [-Werror=unused-variable]
 static const char *client_uuid_linux_ns = "58ac9ca0-2086-4683-a1b8-ec4bc08e01b6";
                    ^~~~~~~~~~~~~~~~~~~~

Fixes: 8e070bc41e1d ("xtest: disable tests 1027 and 1028 if no OpenSSL")
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

host/xtest/regression_1000.c
@@ -2128,6 +2128,7 @@ out:
 	TEEC_CloseSession(&session);
 #else /*!OPENSSL_FOUND*/
 	UNUSED(c);
+	UNUSED(client_uuid_linux_ns);
 	/* xtest_uuid_v5() depends on OpenSSL */
 	Do_ADBG_Log("OpenSSL not available, skipping test 1027");
 #endif