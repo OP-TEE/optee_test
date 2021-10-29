OP-TEE/optee_test

commit 41343db408f028350ef44b1d2127f4ee56b84ad0

xtest: add SDP basic tests
Test setup: non-secure application invokes a TA ("SDP basic" TA) to
inject data into a secure buffer, transform data from/to a secure
buffer and dump data from a secure buffer. Secure buffers are allocated
through ION. Default ION heap tested is the "unmapped" heap: custom+1.

The "SDP basic TA" checks parameter memory attributes and operates
cache synchronization if CFG_CACHE_API is enable.

The non secure application also attempt to map the secure buffer, to
verify its content. This operation may fails. Data read can also be
corrupted as non-secure may fill its cache with data while secure data
are expected fully in physical memory.

Run basic secure buffer access from Trusted Application:
	xtest --sdp-basic

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Zoltan Kuscsik <zoltan.kuscsik@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

host/xtest/sdp_basic.c
@@ -1,0 +1,513 @@
+/*
+ * Copyright (c) 2016, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <tee_client_api.h>
+#include <tee_client_api_extensions.h>
+#include <unistd.h>
+
+#include "include/uapi/linux/ion.h"
+#include "ta_sdp_basic.h"
+#include "crypto_common.h"
+
+/*
+ * SDP basic test setup overview.
+ *
+ * - A dedicated trusted application (SDP basic TA) supports 3 commands:
+ *   - 'inject' data from a nonsecure buffer into a secure buffer
+ *   - 'transform' data inside a secure buffer (bitwise invert + unsigned incr)
+ *   - 'dump' data from a secure buffer into a nonsecure buffer
+
+ * - This test client application (CA) invokes the TA for these 3 operations,
+ *   inject random value, trasforming them then dump them.
+ *
+ * To do so, CA allocates a 'SDP secure buffer' and invoke the TA for these 3
+ * operations (inject then transform then dump) over the allocate buffer.
+ *
+ * The secure buffer is currently allocation through ION support adn
+ * registered to OP-TEE and as shared memory.
+ *
+ * To enhance test coverage against buffer alignement usecase, the CA invokes
+ * the TA with a variable offset inside the buffer. As CA injects random data
+ * into the buffer, the CA uses one of the random bytes to set the value of the
+ * offset in the accessed secure buffer.
+ *
+ * For debugging support, the CA may map (in nonsecure world) the secure
+ * buffer to read its content. As this is unsafe on a hardened platform, this
+ * operation is default disable. When enable, error only print out a warning
+ * trace but does not actually fail the test. This also give an easy way to
+ * check that some HW complains on access violation when nonsecure accesses
+ * secure data.
+ */
+
+static int verbosity = 1;
+
+struct tee_ctx {
+	TEEC_Context ctx;
+	TEEC_Session sess;
+};
+
+/* exported to xtest */
+int allocate_ion_buffer(size_t size, int heap_id);
+
+/* non zero value forces buffer to be mappeable from nonsecure */
+#define BUF_MUST_MAP	0
+
+#define DEFAULT_ION_HEAP_TYPE	ION_HEAP_TYPE_UNMAPPED
+
+int allocate_ion_buffer(size_t size, int heap_id)
+{
+	struct ion_allocation_data alloc_data;
+	struct ion_handle_data hdl_data;
+	struct ion_fd_data fd_data;
+	int ion;
+	int fd = -1;
+
+	ion = open("/dev/ion", O_RDWR);
+	if (ion < 0) {
+		fprintf(stderr, "Error; failed to open /dev/ion\n");
+		verbose("Seems no ION heap is available.\n");
+		verbose("To test ION allocation you can enable\n");
+		verbose("CONFIG_ION and CONFIG_ION_DUMMY in your\n");
+		verbose("linux kernel configuration.\n");
+		return fd;
+	}
+
+	if (heap_id < 0)
+		heap_id = DEFAULT_ION_HEAP_TYPE;
+
+	verbose("Allocate in ION heap '%s'\n",
+		heap_id == ION_HEAP_TYPE_SYSTEM ? "system" :
+		heap_id == ION_HEAP_TYPE_SYSTEM_CONTIG ? "system contig" :
+		heap_id == ION_HEAP_TYPE_CARVEOUT ? "carveout" :
+		heap_id == ION_HEAP_TYPE_CHUNK ? "chunk" :
+		heap_id == ION_HEAP_TYPE_DMA ? "dma" :
+		heap_id == ION_HEAP_TYPE_UNMAPPED ? "unmapped" :
+		"custom");
+
+	alloc_data.len = size;
+	alloc_data.align = 0;
+	alloc_data.flags = 0;
+	alloc_data.heap_id_mask = 1 << heap_id;
+	if (ioctl(ion, ION_IOC_ALLOC, &alloc_data) == -1)
+		goto out;
+
+	fd_data.handle = alloc_data.handle;
+	if (ioctl(ion, ION_IOC_SHARE, &fd_data) != -1)
+		fd = fd_data.fd;
+
+	hdl_data.handle = alloc_data.handle;
+	(void)ioctl(ion, ION_IOC_FREE, &hdl_data);
+out:
+	close(ion);
+	return fd;
+}
+
+static void finalize_tee_ctx(struct tee_ctx *ctx)
+{
+	if (!ctx)
+		return;
+
+	TEEC_CloseSession(&ctx->sess);
+	TEEC_FinalizeContext(&ctx->ctx);
+}
+
+static int create_tee_ctx(struct tee_ctx *ctx)
+{
+	TEEC_Result teerc;
+	TEEC_UUID uuid = TA_SDP_BASIC_UUID;
+	uint32_t err_origin;
+
+	teerc = TEEC_InitializeContext(NULL, &ctx->ctx);
+	if (teerc != TEEC_SUCCESS)
+		return -1;
+
+	teerc = TEEC_OpenSession(&ctx->ctx, &ctx->sess, &uuid,
+			       TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
+	if (teerc != TEEC_SUCCESS)
+		fprintf(stderr, "Error: open session to SDP test TA failed %x %d\n",
+			teerc, err_origin);
+
+	return (teerc == TEEC_SUCCESS) ? 0 : -1;
+}
+
+static int tee_register_buffer(struct tee_ctx *ctx, void **shm_ref, int fd)
+{
+	TEEC_Result teerc;
+	TEEC_SharedMemory *shm;
+
+	shm = malloc(sizeof(*shm));
+	if (!shm)
+		return 1;
+
+	shm->flags = TEEC_MEM_INPUT | TEEC_MEM_OUTPUT;
+	teerc = TEEC_RegisterSharedMemoryFileDescriptor(&ctx->ctx, shm, fd);
+	if (teerc != TEEC_SUCCESS) {
+		fprintf(stderr, "Error: TEEC_RegisterMemoryFileDescriptor() failed %x\n",
+			teerc);
+		return 1;
+	}
+
+	*shm_ref = shm;
+	return 0;
+}
+
+static void tee_deregister_buffer(struct tee_ctx *ctx, void *shm_ref)
+{
+	(void)ctx;
+
+	if (!shm_ref)
+		return;
+
+	TEEC_ReleaseSharedMemory((TEEC_SharedMemory *)shm_ref);
+	free(shm_ref);
+}
+
+static int inject_sdp_data(struct tee_ctx *ctx,
+		    void *in, size_t offset, size_t len, void *shm_ref)
+{
+	TEEC_SharedMemory *shm = (TEEC_SharedMemory *)shm_ref;
+	TEEC_Result teerc;
+	TEEC_Operation op;
+	uint32_t err_origin;
+
+	memset(&op, 0, sizeof(op));
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT,
+					 TEEC_MEMREF_PARTIAL_OUTPUT,
+					 TEEC_NONE, TEEC_NONE);
+
+	op.params[0].tmpref.buffer = in;
+	op.params[0].tmpref.size = len;
+
+	op.params[1].memref.parent = shm;
+	op.params[1].memref.size = len;
+	op.params[1].memref.offset = offset;
+
+	teerc = TEEC_InvokeCommand(&ctx->sess, TA_SDP_BASIC_CMD_INJECT, &op,
+								&err_origin);
+	if (teerc != TEEC_SUCCESS)
+		fprintf(stderr, "Error: invoke SDP test TA (inject) failed %x %d\n",
+			teerc, err_origin);
+
+	return (teerc == TEEC_SUCCESS) ? 0 : -1;
+}
+
+static int transform_sdp_data(struct tee_ctx *ctx,
+			size_t offset, size_t len, void *shm_ref)
+{
+	TEEC_SharedMemory *shm = (TEEC_SharedMemory *)shm_ref;
+	TEEC_Result teerc;
+	TEEC_Operation op;
+	uint32_t err_origin;
+
+	memset(&op, 0, sizeof(op));
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_PARTIAL_INOUT,
+					 TEEC_NONE, TEEC_NONE, TEEC_NONE);
+	op.params[0].memref.parent = shm;
+	op.params[0].memref.size = len;
+	op.params[0].memref.offset = offset;
+
+	teerc = TEEC_InvokeCommand(&ctx->sess, TA_SDP_BASIC_CMD_TRANSFORM, &op,
+								&err_origin);
+	if (teerc != TEEC_SUCCESS)
+		fprintf(stderr, "Error: invoke SDP test TA (transform) failed %x %d\n",
+			teerc, err_origin);
+
+	return (teerc == TEEC_SUCCESS) ? 0 : -1;
+}
+
+static int dump_sdp_data(struct tee_ctx *ctx,
+		  void *out, size_t offset, size_t len, void *shm_ref)
+{
+	TEEC_SharedMemory *shm = (TEEC_SharedMemory *)shm_ref;
+	TEEC_Result teerc;
+	TEEC_Operation op;
+	uint32_t err_origin;
+
+	memset(&op, 0, sizeof(op));
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_PARTIAL_INPUT,
+					 TEEC_MEMREF_TEMP_OUTPUT,
+					 TEEC_NONE, TEEC_NONE);
+	op.params[0].memref.parent = shm;
+	op.params[0].memref.size = len;
+	op.params[0].memref.offset = offset;
+
+	op.params[1].tmpref.buffer = out;
+	op.params[1].tmpref.size = len;
+
+	teerc = TEEC_InvokeCommand(&ctx->sess, TA_SDP_BASIC_CMD_DUMP, &op,
+								&err_origin);
+	if (teerc != TEEC_SUCCESS)
+		fprintf(stderr, "Error: invoke SDP test TA (dump) failed %x %d\n",
+			teerc, err_origin);
+
+	return (teerc == TEEC_SUCCESS) ? 0 : -1;
+}
+
+static int check_sdp_dumped(struct tee_ctx *ctx, void *ref, size_t len,
+								void *out)
+{
+	char *bref = (char *)ref;
+	char *data = (char *)out;
+	int err = 0;
+
+	(void)ctx;
+
+	while(len--)
+		if (*data++ != (unsigned char)(~(*bref++) + 1))
+			err++;
+
+	return err;
+}
+
+/*
+ * Consider 32kByte + 1 of random data is sufficient for an accurate test
+ * whatever the test buffer size is. Random buffer is read as a ring buffer.
+ */
+#define RANDOM_BUFFER_SIZE	(32 * 1024 + 1)
+static int get_random_bytes(char *out, size_t len)
+{
+	static char *rand_buf = NULL;
+	static size_t rand_idx = 0;
+	int rc;
+
+	if (!rand_buf) {
+		const char rand_dev[] = "/dev/urandom";
+		int fd;
+
+		rand_buf = malloc(RANDOM_BUFFER_SIZE);
+		if (!rand_buf) {
+			fprintf(stderr, "failed to random buffer memory (%d bytes)\n",
+				RANDOM_BUFFER_SIZE);
+			return -1;
+		}
+
+		fd = open(rand_dev, O_RDONLY);
+		if (fd < 0) {
+			fprintf(stderr, "failed to open %s\n", rand_dev);
+			return -1;
+		}
+
+		rc = read(fd, rand_buf, RANDOM_BUFFER_SIZE);
+		if (rc != RANDOM_BUFFER_SIZE) {
+			fprintf(stderr, "failed to read %d bytes from %s\n",
+				RANDOM_BUFFER_SIZE, rand_dev);
+			return -1;
+		}
+		close(fd);
+	}
+
+	while (len) {
+		size_t t_len = (RANDOM_BUFFER_SIZE < len) ? RANDOM_BUFFER_SIZE : len;
+
+		if ((rand_idx + t_len) > RANDOM_BUFFER_SIZE) {
+			int sz_end = RANDOM_BUFFER_SIZE - rand_idx;
+			int sz_beg = t_len - sz_end;
+
+			memcpy(out, rand_buf + rand_idx, sz_end);
+			memcpy(out + sz_end, rand_buf , sz_beg);
+			rand_idx = sz_beg;
+		} else {
+			memcpy(out, rand_buf + rand_idx, t_len);
+			rand_idx += t_len;
+		}
+		len -= t_len;
+	}
+	return 0;
+}
+
+
+static int sdp_basic_test(size_t size, size_t loop, int ion_heap, int rnd_offset)
+{
+	struct tee_ctx *ctx = NULL;
+	unsigned char *test_buf = NULL;
+	unsigned char *ref_buf = NULL;
+	void *shm_ref = NULL;
+	unsigned int err = 1;
+	int fd = -1;
+	size_t sdp_size = size;
+	size_t offset;
+
+	if (!loop) {
+		fprintf(stderr, "Error: null loop value\n");
+		return 1;
+	}
+
+	/* reduce size to enable offset tests (max offset is 255 bytes) */
+	if (rnd_offset)
+		size -= 255;
+
+	test_buf = malloc(size);
+	ref_buf = malloc(size);
+	if (!test_buf || !ref_buf) {
+		verbose("failed to allocate memory\n");
+		goto out;
+	}
+
+	fd = allocate_ion_buffer(sdp_size, ion_heap);
+	if (fd < 0) {
+		verbose("Failed to allocate SDP buffer (%lu bytes) in ION heap %d: %d\n",
+				sdp_size, ion_heap, fd);
+		goto out;
+	}
+
+	/* register secure buffer to TEE */
+	ctx = malloc(sizeof(*ctx));
+	if (!ctx)
+		goto out;
+	if (create_tee_ctx(ctx))
+		goto out;
+	if (tee_register_buffer(ctx, &shm_ref, fd))
+		goto out;
+
+	/* release registered fd: tee should still hold refcount on resource */
+	close(fd);
+	fd = -1;
+
+	/* invoke trusted application with secure buffer as memref parameter */
+	while (loop--) {
+		/* get an buffer of random-like values */
+		if (get_random_bytes((char *)ref_buf, size))
+			goto out;
+		memcpy(test_buf, ref_buf, size);
+		/* random offset [0 255] */
+		offset = (unsigned int)*ref_buf;
+
+		/* TA writes into SDP buffer */
+		if (inject_sdp_data(ctx, test_buf, offset, size, shm_ref))
+			goto out;
+
+		/* TA reads/writes into SDP buffer */
+		if (transform_sdp_data(ctx, offset, size, shm_ref))
+			goto out;
+
+		/* TA reads into SDP buffer */
+		if (dump_sdp_data(ctx, test_buf, offset, size, shm_ref))
+			goto out;
+
+		/* check dumped data are the expected ones */
+		if (check_sdp_dumped(ctx, ref_buf, size, test_buf)) {
+			fprintf(stderr, "check SDP data: %d errors\n", err);
+			goto out;
+		}
+	}
+	err = 0;
+	verbose("%s: successed\n", __func__);
+out:
+	if (err)
+		verbose("test failed\n");
+	if (fd >= 0)
+		close(fd);
+	if (shm_ref)
+		tee_deregister_buffer(ctx, shm_ref);
+	finalize_tee_ctx(ctx);
+	free(ctx);
+	free(ref_buf);
+	free(test_buf);
+	return err;
+}
+
+#define _TO_STR(x) #x
+#define TO_STR(x) _TO_STR(x)
+
+static void usage(const char *progname, size_t size, int loop, int ion_heap)
+{
+	fprintf(stderr, "Usage: %s [OPTION]\n", progname);
+	fprintf(stderr,
+		"Testing basic accesses to secure buffer (SDP) on OP-TEE.\n"
+		"Allocates a secure buffer and invoke a TA to access it.\n"
+		"TA is used to init/transform/dump the secure buffer.\n"
+		"CA check dumped content.\n\n");
+
+	fprintf(stderr, "Options:\n");
+	fprintf(stderr, " -h|--help      Print this help and exit\n");
+	fprintf(stderr, " -v             Be verbose\n");
+	fprintf(stderr, " -s SIZE        SDP buffer byte size [%zu]\n", size);
+	fprintf(stderr, " -n LOOP        Test loop iterations [%u]\n", loop);
+	fprintf(stderr, " --ion-heap ID  Target ION heap ID [%d]\n", ion_heap);
+	fprintf(stderr, " --no-offset    No random offset [0 255] in buffer\n");
+}
+
+#define NEXT_ARG(i) \
+	do { \
+		if (++i == argc) { \
+			fprintf(stderr, "%s: %s: missing argument\n", \
+				argv[0], argv[i-1]); \
+			return 1; \
+		} \
+	} while (0);
+
+int sdp_basic_runner_cmd_parser(int argc, char *argv[])
+{
+	size_t test_size = 5000;
+	size_t test_loop = 1000;
+	int ion_heap = DEFAULT_ION_HEAP_TYPE;
+	int rnd_offset = 1;
+	int i;
+
+	/* Parse command line */
+	for (i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
+			usage(argv[0], test_size, test_loop, ion_heap);
+			return 0;
+		}
+	}
+	for (i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "-v")) {
+			verbosity++;
+		} else if (!strcmp(argv[i], "-s")) {
+			NEXT_ARG(i);
+			test_size = atoi(argv[i]);
+		} else if (!strcmp(argv[i], "-n")) {
+			NEXT_ARG(i);
+			test_loop = atoi(argv[i]);
+		} else if (!strcmp(argv[i], "--ion-heap")) {
+			NEXT_ARG(i);
+			ion_heap = atoi(argv[i]);
+		} else if (!strcmp(argv[i], "--no-offset")) {
+			rnd_offset = 0;
+		} else {
+			fprintf(stderr, "%s: invalid argument: %s\n",
+				argv[0], argv[i]);
+			usage(argv[0], test_size, test_loop, ion_heap);
+			return 1;
+		}
+	}
+
+	verbose("Secure Data Path basic accesses from trusted applications\n");
+
+	if (sdp_basic_test(test_size, test_loop, ion_heap, rnd_offset))
+		return 1;
+
+	return 0;
+}

Android.mk
@@ -49,6 +49,10 @@ srcs += xtest_4000.c \
 	aes_perf.c \
 	sha_perf.c
 
+ifeq ($(CFG_SECURE_DATA_PATH),y)
+srcs += sdp_basic.c
+endif
+
 LOCAL_SRC_FILES := $(patsubst %,host/xtest/%,$(srcs))
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/host/xtest \
@@ -67,7 +71,8 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/host/xtest \
 		$(LOCAL_PATH)/ta/storage_benchmark/include \
 		$(LOCAL_PATH)/ta/sha_perf/include \
 		$(LOCAL_PATH)/ta/aes_perf/include \
-		$(LOCAL_PATH)/ta/socket/include
+		$(LOCAL_PATH)/ta/socket/include \
+		$(LOCAL_PATH)/ta/sdp_basic/include
 
 # Include configuration file generated by OP-TEE OS (CFG_* macros)
 LOCAL_CFLAGS += -include conf.h

host/xtest/Makefile
@@ -56,6 +56,10 @@ srcs += xtest_4000.c \
 	adbg/src/adbg_run.c \
 	adbg/src/security_utils_hex.c
 
+ifeq ($(CFG_SECURE_DATA_PATH),y)
+srcs += sdp_basic.c
+endif
+
 ifdef CFG_GP_PACKAGE_PATH
 CFLAGS += -DWITH_GP_TESTS
 
@@ -87,6 +91,8 @@ CFLAGS += -I../../ta/concurrent_large/include
 CFLAGS += -I../../ta/sha_perf/include
 CFLAGS += -I../../ta/aes_perf/include
 CFLAGS += -I../../ta/socket/include
+CFLAGS += -I../../ta/sdp_basic/include
+
 ifdef CFG_GP_PACKAGE_PATH
 CFLAGS += -I../../ta/GP_TTA_Arithmetical
 CFLAGS += -I../../ta/GP_TTA_Crypto

host/xtest/include/uapi/linux/ion.h
@@ -1,0 +1,238 @@
+/*
+ * drivers/staging/android/uapi/ion.h
+ *
+ * Copyright (C) 2011 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _UAPI_LINUX_ION_H
+#define _UAPI_LINUX_ION_H
+
+#include <linux/ioctl.h>
+#include <linux/types.h>
+
+typedef int ion_user_handle_t;
+
+/**
+ * enum ion_heap_types - list of all possible types of heaps
+ * @ION_HEAP_TYPE_SYSTEM:	 memory allocated via vmalloc
+ * @ION_HEAP_TYPE_SYSTEM_CONTIG: memory allocated via kmalloc
+ * @ION_HEAP_TYPE_CARVEOUT:	 memory allocated from a prereserved
+ *				 carveout heap, allocations are physically
+ *				 contiguous
+ * @ION_HEAP_TYPE_DMA:		 memory allocated via DMA API
+ * @ION_HEAP_TYPE_UNMAPPED:	 memory not mappable into linux address space
+ * @ION_NUM_HEAPS:		 helper for iterating over heaps, a bit mask
+ *				 is used to identify the heaps, so only 32
+ *				 total heap types are supported
+ */
+enum ion_heap_type {
+	ION_HEAP_TYPE_SYSTEM,
+	ION_HEAP_TYPE_SYSTEM_CONTIG,
+	ION_HEAP_TYPE_CARVEOUT,
+	ION_HEAP_TYPE_CHUNK,
+	ION_HEAP_TYPE_DMA,
+	ION_HEAP_TYPE_UNMAPPED,
+	ION_HEAP_TYPE_CUSTOM, /*
+			       * must be last so device specific heaps always
+			       * are at the end of this enum
+			       */
+};
+
+#define ION_NUM_HEAP_IDS		(sizeof(unsigned int) * 8)
+
+/**
+ * allocation flags - the lower 16 bits are used by core ion, the upper 16
+ * bits are reserved for use by the heaps themselves.
+ */
+
+/*
+ * mappings of this buffer should be cached, ion will do cache maintenance
+ * when the buffer is mapped for dma
+ */
+#define ION_FLAG_CACHED 1
+
+/*
+ * mappings of this buffer will created at mmap time, if this is set
+ * caches must be managed manually
+ */
+#define ION_FLAG_CACHED_NEEDS_SYNC 2
+
+/**
+ * DOC: Ion Userspace API
+ *
+ * create a client by opening /dev/ion
+ * most operations handled via following ioctls
+ *
+ */
+
+/**
+ * struct ion_allocation_data - metadata passed from userspace for allocations
+ * @len:		size of the allocation
+ * @align:		required alignment of the allocation
+ * @heap_id_mask:	mask of heap ids to allocate from
+ * @flags:		flags passed to heap
+ * @handle:		pointer that will be populated with a cookie to use to
+ *			refer to this allocation
+ *
+ * Provided by userspace as an argument to the ioctl
+ */
+struct ion_allocation_data {
+	size_t len;
+	size_t align;
+	unsigned int heap_id_mask;
+	unsigned int flags;
+	ion_user_handle_t handle;
+};
+
+/**
+ * struct ion_fd_data - metadata passed to/from userspace for a handle/fd pair
+ * @handle:	a handle
+ * @fd:		a file descriptor representing that handle
+ *
+ * For ION_IOC_SHARE or ION_IOC_MAP userspace populates the handle field with
+ * the handle returned from ion alloc, and the kernel returns the file
+ * descriptor to share or map in the fd field.  For ION_IOC_IMPORT, userspace
+ * provides the file descriptor and the kernel returns the handle.
+ */
+struct ion_fd_data {
+	ion_user_handle_t handle;
+	int fd;
+};
+
+/**
+ * struct ion_handle_data - a handle passed to/from the kernel
+ * @handle:	a handle
+ */
+struct ion_handle_data {
+	ion_user_handle_t handle;
+};
+
+/**
+ * struct ion_custom_data - metadata passed to/from userspace for a custom ioctl
+ * @cmd:	the custom ioctl function to call
+ * @arg:	additional data to pass to the custom ioctl, typically a user
+ *		pointer to a predefined structure
+ *
+ * This works just like the regular cmd and arg fields of an ioctl.
+ */
+struct ion_custom_data {
+	unsigned int cmd;
+	unsigned long arg;
+};
+
+#define MAX_HEAP_NAME			32
+
+/**
+ * struct ion_heap_data - data about a heap
+ * @name - first 32 characters of the heap name
+ * @type - heap type
+ * @heap_id - heap id for the heap
+ */
+struct ion_heap_data {
+	char name[MAX_HEAP_NAME];
+	__u32 type;
+	__u32 heap_id;
+	__u32 reserved0;
+	__u32 reserved1;
+	__u32 reserved2;
+};
+
+/**
+ * struct ion_heap_query - collection of data about all heaps
+ * @cnt - total number of heaps to be copied
+ * @heaps - buffer to copy heap data
+ */
+struct ion_heap_query {
+	__u32 cnt; /* Total number of heaps to be copied */
+	__u32 reserved0; /* align to 64bits */
+	__u64 heaps; /* buffer to be populated */
+	__u32 reserved1;
+	__u32 reserved2;
+};
+
+#define ION_IOC_MAGIC		'I'
+
+/**
+ * DOC: ION_IOC_ALLOC - allocate memory
+ *
+ * Takes an ion_allocation_data struct and returns it with the handle field
+ * populated with the opaque handle for the allocation.
+ */
+#define ION_IOC_ALLOC		_IOWR(ION_IOC_MAGIC, 0, \
+				      struct ion_allocation_data)
+
+/**
+ * DOC: ION_IOC_FREE - free memory
+ *
+ * Takes an ion_handle_data struct and frees the handle.
+ */
+#define ION_IOC_FREE		_IOWR(ION_IOC_MAGIC, 1, struct ion_handle_data)
+
+/**
+ * DOC: ION_IOC_MAP - get a file descriptor to mmap
+ *
+ * Takes an ion_fd_data struct with the handle field populated with a valid
+ * opaque handle.  Returns the struct with the fd field set to a file
+ * descriptor open in the current address space.  This file descriptor
+ * can then be used as an argument to mmap.
+ */
+#define ION_IOC_MAP		_IOWR(ION_IOC_MAGIC, 2, struct ion_fd_data)
+
+/**
+ * DOC: ION_IOC_SHARE - creates a file descriptor to use to share an allocation
+ *
+ * Takes an ion_fd_data struct with the handle field populated with a valid
+ * opaque handle.  Returns the struct with the fd field set to a file
+ * descriptor open in the current address space.  This file descriptor
+ * can then be passed to another process.  The corresponding opaque handle can
+ * be retrieved via ION_IOC_IMPORT.
+ */
+#define ION_IOC_SHARE		_IOWR(ION_IOC_MAGIC, 4, struct ion_fd_data)
+
+/**
+ * DOC: ION_IOC_IMPORT - imports a shared file descriptor
+ *
+ * Takes an ion_fd_data struct with the fd field populated with a valid file
+ * descriptor obtained from ION_IOC_SHARE and returns the struct with the handle
+ * filed set to the corresponding opaque handle.
+ */
+#define ION_IOC_IMPORT		_IOWR(ION_IOC_MAGIC, 5, struct ion_fd_data)
+
+/**
+ * DOC: ION_IOC_SYNC - syncs a shared file descriptors to memory
+ *
+ * Deprecated in favor of using the dma_buf api's correctly (syncing
+ * will happen automatically when the buffer is mapped to a device).
+ * If necessary should be used after touching a cached buffer from the cpu,
+ * this will make the buffer in memory coherent.
+ */
+#define ION_IOC_SYNC		_IOWR(ION_IOC_MAGIC, 7, struct ion_fd_data)
+
+/**
+ * DOC: ION_IOC_CUSTOM - call architecture specific ion ioctl
+ *
+ * Takes the argument of the architecture specific ioctl to call and
+ * passes appropriate userdata for that ioctl
+ */
+#define ION_IOC_CUSTOM		_IOWR(ION_IOC_MAGIC, 6, struct ion_custom_data)
+
+/**
+ * DOC: ION_IOC_HEAP_QUERY - information about available heaps
+ *
+ * Takes an ion_heap_query structure and populates information about
+ * available Ion heaps.
+ */
+#define ION_IOC_HEAP_QUERY     _IOWR(ION_IOC_MAGIC, 8, \
+					struct ion_heap_query)
+
+#endif /* _UAPI_LINUX_ION_H */

host/xtest/xtest_main.c
@@ -54,6 +54,10 @@ void usage(char *program)
 	printf("\t--aes-perf         AES performance testing tool for OP-TEE\n");
 	printf("\t--aes perf -h      show usage of AES performance testing tool\n");
 	printf("\n");
+#ifdef CFG_SECURE_DATA_PATH
+	printf("\t--sdp-basic        Basic Secure Data Path test setup for OP-TEE ('-h' for usage)\n");
+#endif
+	printf("\n");
 }
 
 int main(int argc, char *argv[])
@@ -76,6 +80,10 @@ int main(int argc, char *argv[])
 		return sha_perf_runner_cmd_parser(argc-1, &argv[1]);
 	else if (argc > 1 && !strcmp(argv[1], "--aes-perf"))
 		return aes_perf_runner_cmd_parser(argc-1, &argv[1]);
+#ifdef CFG_SECURE_DATA_PATH
+	else if (argc > 1 && !strcmp(argv[1], "--sdp-basic"))
+		return sdp_basic_runner_cmd_parser(argc-1, &argv[1]);
+#endif
 
 	while ((opt = getopt(argc, argv, "d:l:t:h")) != -1)
 		switch (opt) {

ta/Makefile
@@ -26,6 +26,10 @@ TA_DIRS := create_fail_test \
 	   aes_perf \
 	   socket
 
+ifeq ($(CFG_SECURE_DATA_PATH),y)
+TA_DIRS += sdp_basic
+endif
+
 ifdef CFG_GP_PACKAGE_PATH
 TA_DIRS += GP_TTA_Arithmetical \
 	   GP_TTA_Crypto \

ta/sdp_basic/Android.mk
@@ -1,0 +1,4 @@
+LOCAL_PATH := $(call my-dir)
+
+local_module := 12345678-5b69-11e4-9dbb-101f74f00099.ta
+include $(BUILD_OPTEE_MK)

ta/sdp_basic/Makefile
@@ -1,0 +1,8 @@
+BINARY = 12345678-5b69-11e4-9dbb-101f74f00099
+
+ifeq ($(CFG_CACHE_API),y)
+CFLAGS += -DCFG_CACHE_API=y
+endif
+
+include ../ta_common.mk
+

commit bd19054741642ab084947beb983d79f6458a9bd0

xtest: sdp: fix resource leak in get_random_bytes()
Close file descriptor before returning when failing to read
from /dev/urandom.

Fixes: 41343db4 ("xtest: add SDP basic tests")
Signed-off-by: Cyrille Fleury <cyrille.fleury@nxp.com>
Reviewed-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

host/xtest/sdp_basic.c
@@ -402,6 +402,7 @@ static int get_random_bytes(char *out, size_t len)
 		if (rc != RANDOM_BUFFER_SIZE) {
 			fprintf(stderr, "failed to read %d bytes from %s\n",
 				RANDOM_BUFFER_SIZE, rand_dev);
+			close(fd);
 			return -1;
 		}
 		close(fd);