1c6a2dbbd01fd7dccaae2d4c90963e9e87b41617
Ruchika Gupta <ruchika.gupta@linaro.org>
2021-12-17 11:32:46 +0530

    Disable C++ file compilation when WITH_CXX_TESTS=n
    Disable the compilation of C++ files from os_test_lib
    and os_test_lib_dl if C++ tests are disabled i.e xtests
    are built with option WITH_CXX_TESTS=n.
    
    Signed-off-by: Ruchika Gupta <ruchika.gupta@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Jerome Forissier <jerome@forissier.org>

--- ta/os_test_lib/sub.mk
+++ ta/os_test_lib/sub.mk
@@ -1,6 +1,8 @@
 cppflags-$(WITH_TLS_TESTS) += -DWITH_TLS_TESTS=1
 global-incdirs-y += include
 srcs-y += os_test_lib.c
+ifeq ($(WITH_CXX_TESTS),y)
 ifneq ($(COMPILER),clang)
 srcs-y += os_test_lib_cxx.cpp
+endif
 endif
--- ta/os_test_lib_dl/sub.mk
+++ ta/os_test_lib_dl/sub.mk
@@ -1,5 +1,7 @@
 global-incdirs-y += include
 srcs-y += os_test_lib_dl.c
+ifeq ($(WITH_CXX_TESTS),y)
 ifneq ($(COMPILER),clang)
 srcs-y += os_test_lib_dl_cxx.cpp
+endif
 endif
