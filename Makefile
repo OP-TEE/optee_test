OP-TEE/optee_test

commit 2a1ef2c76fa9c6fd0ec8049e1267555368f8b246

Add install target
It is pretty common to have an install target that can be used like
this [1]:

  make install DESTDIR=/some/dir

This comes in handy to install into a local root FS directory that
would be exported via NFS for instance.

[1] https://www.gnu.org/prep/standards/html_node/DESTDIR.html

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

Makefile
@@ -10,8 +10,10 @@ endif
 
 ifneq ($V,1)
 	q := @
+	echo := @echo
 else
 	q :=
+	echo := @:
 endif
 
 .PHONY: all
@@ -263,3 +265,11 @@ else
 patch:
 	$(q) echo "Please define CFG_GP_PACKAGE_PATH" && false
 endif
+
+install:
+	$(echo) '  INSTALL ${DESTDIR}/lib/optee_armtz'
+	$(q)mkdir -p ${DESTDIR}/lib/optee_armtz
+	$(q)find $(out-dir) -name \*.ta -exec cp -a {} ${DESTDIR}/lib/optee_armtz \;
+	$(echo) '  INSTALL ${DESTDIR}/bin'
+	$(q)mkdir -p ${DESTDIR}/bin
+	$(q)cp -a $(out-dir)/xtest/xtest ${DESTDIR}/bin

commit 09fddeb2a0fe759a556f71dfc231de0b39d128c9

Makefile: fix ownership contamination when installing output files
When installing output files with the `install` Makefile target, the
output file ownership is preserved because the file copy to the
destination directory is done with `cp -a/--archive`.

When using the `install` Makefile target in a Yocto recipe, it triggers
the following Bitbake host contamination warning:

WARNING: optee-test-3.17.0.imx-r0 do_package_qa: QA Issue: optee-test: /lib/optee_armtz/b689f2a7-8adf-477a-9f99-32e90c0ad0a2.ta is owned by uid 1001, which is the same as the user running bitbake. This may be due to host contamination
optee-test: /lib/optee_armtz/5ce0c432-0ab0-40e5-a056-782ca0e6aba2.ta is owned by uid 1001, which is the same as the user running bitbake.  This may be due to host contamination

`cp` instead of `cp -a` solves the issue.

Fixes: 2a1ef2c76f ("Add install target")
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

Makefile
@@ -75,10 +75,10 @@ checkpatch-staging:
 install:
 	$(echo) '  INSTALL ${DESTDIR}/lib/optee_armtz'
 	$(q)mkdir -p ${DESTDIR}/lib/optee_armtz
-	$(q)find $(out-dir) -name \*.ta -exec cp -a {} ${DESTDIR}/lib/optee_armtz \;
+	$(q)find $(out-dir) -name \*.ta -exec cp {} ${DESTDIR}/lib/optee_armtz \;
 	$(echo) '  INSTALL ${DESTDIR}/bin'
 	$(q)mkdir -p ${DESTDIR}/bin
-	$(q)cp -a $(out-dir)/xtest/xtest ${DESTDIR}/bin
+	$(q)cp $(out-dir)/xtest/xtest ${DESTDIR}/bin
 	$(echo) '  INSTALL ${DESTDIR}/$(CFG_TEE_PLUGIN_LOAD_PATH)'
 	$(q)mkdir -p ${DESTDIR}/$(CFG_TEE_PLUGIN_LOAD_PATH)
 	$(q)cp $(out-dir)/supp_plugin/*.plugin ${DESTDIR}/$(CFG_TEE_PLUGIN_LOAD_PATH)