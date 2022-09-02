825035dc9f5b1f91ce4e990503c1381aa10fa133
Jens Wiklander <jens.wiklander@linaro.org>
2022-09-02 13:52:04 +0200

    ta: add subkey test TAs
    A few subkeys and TAs are added to test different subkey cases. For
    details on the subkeys see subkey_notes.txt
    
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ ta/subkey1/include/user_ta_header_defines.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2022, Linaro Limited
+ */
+
+#ifndef USER_TA_HEADER_DEFINES_H
+#define USER_TA_HEADER_DEFINES_H
+
+#include <ta_subkey1.h>
+#include <user_ta_header.h>
+
+#define TA_UUID TA_SUBKEY1_UUID
+
+#define TA_FLAGS	(TA_FLAG_MULTI_SESSION)
+#define TA_STACK_SIZE	(2 * 1024)
+#define TA_DATA_SIZE	(2 * 1024)
+
+#endif /*USER_TA_HEADER_DEFINES_H*/
---
+++ ta/subkey2/ta_entry.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2022, Linaro Limited
+ */
+
+#include <ta_subkey2.h>
+#include <tee_internal_api.h>
+
+TEE_Result TA_CreateEntryPoint(void)
+{
+	return TEE_SUCCESS;
+}
+
+void TA_DestroyEntryPoint(void)
+{
+}
+
+TEE_Result TA_OpenSessionEntryPoint(uint32_t param_types __unused,
+				    TEE_Param params[4] __unused,
+				    void **session_ctx __unused)
+{
+	return TEE_SUCCESS;
+}
+
+void TA_CloseSessionEntryPoint(void *session_ctx __unused)
+{
+}
+
+TEE_Result TA_InvokeCommandEntryPoint(void *session_ctx __unused,
+				      uint32_t cmd_id __unused,
+				      uint32_t param_types __unused,
+				      TEE_Param params[4] __unused)
+{
+	return TEE_SUCCESS;
+}
