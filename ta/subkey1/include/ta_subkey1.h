86c3b07453f54837a736e97d3f99087df69cbdd8
Jens Wiklander <jens.wiklander@linaro.org>
2022-09-02 13:52:04 +0200

    ta: add subkey test TAs
    A few subkeys and TAs are added to test different subkey cases. For
    details on the subkeys see subkey_notes.txt
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ ta/subkey1/include/ta_subkey1.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2021, Linaro Limited
+ */
+
+#ifndef TA_SUBKEY1_H
+#define TA_SUBKEY1_H
+
+#define TA_SUBKEY1_UUID { 0xdcdfecce, 0xf5a0, 0x554a, \
+	{ 0xb9, 0x6b, 0x74, 0xa3, 0xe5, 0x07, 0xfc, 0xff } }
+
+#endif /*TA_SUBKEY1_H */
---
+++ ta/subkey2/include/ta_subkey2.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2021, Linaro Limited
+ */
+
+#ifndef TA_SUBKEY2_H
+#define TA_SUBKEY2_H
+
+#define TA_SUBKEY2_UUID { 0xa720ccbb, 0x51da, 0x417d, \
+	{ 0xb8, 0x2e, 0xe5, 0x44, 0x5d, 0x47, 0x4a, 0x7a } }
+
+#endif /*TA_SUBKEY2_H */
