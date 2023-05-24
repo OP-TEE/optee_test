OP-TEE/optee_test

commit b38e9e06fcbd30957ec2b9d1429d57a310cce21f

xtest: pkcs11: add PKCS11 ACL auth test
New PKCS11 test for TEE identity based authentication is added.
C_InitToken with null pointer creates protected authentication
path and then C_InitPin setup the ACL based user pin.
Once initialization is completed, the current connection is closed
and a new CKU_USER connection is opened. And then, login/logout test
with ACL authentication is performed.

Signed-off-by: Matthew(Sukyoung) Chae <matthewc@axis.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>

host/xtest/xtest_uuid_helpers.c
@@ -82,6 +82,26 @@ out:
 	return res;
 }
 
+#define UUID_STR_LEN	37
+
+char *xtest_uuid_to_allocated_str(const TEEC_UUID *uuid)
+{
+	char *s = NULL;
+
+	s = malloc(UUID_STR_LEN);
+	if (!s)
+		return NULL;
+
+	sprintf(s, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x", uuid->timeLow,
+		uuid->timeMid, uuid->timeHiAndVersion, uuid->clockSeqAndNode[0],
+		uuid->clockSeqAndNode[1], uuid->clockSeqAndNode[2],
+		uuid->clockSeqAndNode[3], uuid->clockSeqAndNode[4],
+		uuid->clockSeqAndNode[5], uuid->clockSeqAndNode[6],
+		uuid->clockSeqAndNode[7]);
+
+	return s;
+}
+
 #ifdef OPENSSL_FOUND
 TEEC_Result xtest_uuid_v5(TEEC_UUID *uuid, const TEEC_UUID *ns,
 			  const void *name, size_t size)

host/xtest/xtest_uuid_helpers.h
@@ -17,6 +17,13 @@
  */
 TEEC_Result xtest_uuid_from_str(TEEC_UUID *uuid, const char *s);
 
+/*
+ * Convert a TEEC_UUID @uuid into an allocated UUID string @return
+ * Expected format for @return is: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+ * 'x' being any hexadecimal digit (0-9a-fA-F)
+ */
+char *xtest_uuid_to_allocated_str(const TEEC_UUID *uuid);
+
 #ifdef OPENSSL_FOUND
 /*
  * Form UUIDv5 from given name space and name.

commit 11fa15bf3a4f5b21d483946cd48781ed4e3acd60

xtest: fix compilation error with WITH_OPENSSL=n
Fix the following compilation error:
$ CROSS_COMPILE=... OPTEE_CLIENT_EXPORT=... TA_DEV_KIT_DIR=... \
  WITH_OPENSSL=n make

/optee_test/host/xtest/xtest_uuid_helpers.c: In function ‘xtest_uuid_to_allocated_str’:
/optee_test/host/xtest/xtest_uuid_helpers.c:91:13: error: implicit declaration of function ‘malloc’ [-Werror=implicit-function-declaration]
   91 |         s = malloc(UUID_STR_LEN);
      |             ^~~~~~
/optee_test/host/xtest/xtest_uuid_helpers.c:16:1: note: include ‘<stdlib.h>’ or provide a declaration of ‘malloc’
   15 | #include "xtest_uuid_helpers.h"
  +++ |+#include <stdlib.h>
   16 |
/optee_test/host/xtest/xtest_uuid_helpers.c:91:13: error: incompatible implicit declaration of built-in function ‘malloc’ [-Werror=builtin-declaration-mismatch]
   91 |         s = malloc(UUID_STR_LEN);
      |             ^~~~~~
/optee_test/host/xtest/xtest_uuid_helpers.c:91:13: note: include ‘<stdlib.h>’ or provide a declaration of ‘malloc’
/optee_test/host/xtest/xtest_uuid_helpers.c:95:9: error: implicit declaration of function ‘sprintf’ [-Werror=implicit-function-declaration]
   95 |         sprintf(s, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x", uuid->timeLow,
      |         ^~~~~~~
/optee_test/host/xtest/xtest_uuid_helpers.c:16:1: note: include ‘<stdio.h>’ or provide a declaration of ‘sprintf’
   15 | #include "xtest_uuid_helpers.h"
  +++ |+#include <stdio.h>
   16 |
/optee_test/host/xtest/xtest_uuid_helpers.c:95:9: error: incompatible implicit declaration of built-in function ‘sprintf’ [-Werror=builtin-declaration-mismatch]
   95 |         sprintf(s, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x", uuid->timeLow,
      |         ^~~~~~~
/optee_test/host/xtest/xtest_uuid_helpers.c:95:9: note: include ‘<stdio.h>’ or provide a declaration of ‘sprintf’
cc1: all warnings being treated as errors

Fixes: b38e9e06fcbd ("xtest: pkcs11: add PKCS11 ACL auth test")

host/xtest/xtest_uuid_helpers.c
@@ -9,6 +9,8 @@
 #include <openssl/evp.h>
 #endif
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <tee_api_types.h>
 #include <tee_client_api.h>