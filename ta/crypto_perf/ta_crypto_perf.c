OP-TEE/optee_test

commit e18381f54eb86326f64c0cbe472e417decb8be39

xtest: combine aes_perf and hash_perf TAs into crypto_perf
There is quite a bit of copy in these files. So, I consolidate the
code to make it easier to maintain.

Signed-off-by: Zexi Yu <yuzexi@hisilicon.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
[jf: edit commit subject, fix whitespace in ta_crypto_perf_priv.h]
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

ta/crypto_perf/ta_crypto_perf.c
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (c) 2015, Linaro Limited
- * All rights reserved.
  */
 
+#include <string.h>
 #include <tee_internal_api_extensions.h>
 #include <tee_internal_api.h>
 #include <tee_ta_api.h>
-#include <string.h>
 #include <trace.h>
+#include <utee_defines.h>
+#include <util.h>
 
-#include "ta_aes_perf.h"
-#include "ta_aes_perf_priv.h"
+#include "ta_crypto_perf.h"
+#include "ta_crypto_perf_priv.h"
 
 #define CHECK(res, name, action) do {			\
 		if ((res) != TEE_SUCCESS) {		\
@@ -26,7 +27,7 @@ static uint8_t iv[] = { 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7,
 			0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF };
 static int use_iv;
 
-static TEE_OperationHandle crypto_op = NULL;
+static TEE_OperationHandle crypto_op;
 static uint32_t algo;
 
 static bool is_inbuf_a_secure_memref(TEE_Param *param)
@@ -80,7 +81,7 @@ static __maybe_unused TEE_Result flush_memref_buffer(TEE_Param *param __unused)
 }
 #endif /* CFG_CACHE_API */
 
-TEE_Result cmd_process(uint32_t param_types,
+TEE_Result cmd_cipher_process(uint32_t param_types,
 		       TEE_Param params[TEE_NUM_PARAMS],
 		       bool use_sdp)
 {
@@ -167,18 +168,19 @@ TEE_Result cmd_process(uint32_t param_types,
 	return TEE_SUCCESS;
 }
 
-TEE_Result cmd_prepare_key(uint32_t param_types, TEE_Param params[4])
+TEE_Result cmd_cipher_prepare_key(uint32_t param_types, TEE_Param params[4])
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
 	TEE_ObjectHandle hkey = TEE_HANDLE_NULL;
 	TEE_ObjectHandle hkey2 = TEE_HANDLE_NULL;
+	TEE_ObjectType objectType;
 	TEE_Attribute attr = { };
 	uint32_t mode = 0;
 	uint32_t op_keysize = 0;
 	uint32_t keysize = 0;
 	const uint8_t *ivp = NULL;
 	size_t ivlen = 0;
-	static uint8_t aes_key[] = { 0x00, 0x01, 0x02, 0x03,
+	static uint8_t cipher_key[] = { 0x00, 0x01, 0x02, 0x03,
 				     0x04, 0x05, 0x06, 0x07,
 				     0x08, 0x09, 0x0A, 0x0B,
 				     0x0C, 0x0D, 0x0E, 0x0F,
@@ -186,7 +188,7 @@ TEE_Result cmd_prepare_key(uint32_t param_types, TEE_Param params[4])
 				     0x14, 0x15, 0x16, 0x17,
 				     0x18, 0x19, 0x1A, 0x1B,
 				     0x1C, 0x1D, 0x1E, 0x1F };
-	static uint8_t aes_key2[] = { 0x20, 0x21, 0x22, 0x23,
+	static uint8_t cipher_key2[] = { 0x20, 0x21, 0x22, 0x23,
 				      0x24, 0x25, 0x26, 0x27,
 				      0x28, 0x29, 0x2A, 0x2B,
 				      0x2C, 0x2D, 0x2E, 0x2F,
@@ -209,23 +211,28 @@ TEE_Result cmd_prepare_key(uint32_t param_types, TEE_Param params[4])
 	switch (params[1].value.a) {
 	case TA_AES_ECB:
 		algo = TEE_ALG_AES_ECB_NOPAD;
+		objectType = TEE_TYPE_AES;
 		use_iv = 0;
 		break;
 	case TA_AES_CBC:
 		algo = TEE_ALG_AES_CBC_NOPAD;
+		objectType = TEE_TYPE_AES;
 		use_iv = 1;
 		break;
 	case TA_AES_CTR:
 		algo = TEE_ALG_AES_CTR;
+		objectType = TEE_TYPE_AES;
 		use_iv = 1;
 		break;
 	case TA_AES_XTS:
 		algo = TEE_ALG_AES_XTS;
+		objectType = TEE_TYPE_AES;
 		use_iv = 1;
 		op_keysize *= 2;
 		break;
 	case TA_AES_GCM:
 		algo = TEE_ALG_AES_GCM;
+		objectType = TEE_TYPE_AES;
 		use_iv = 1;
 		break;
 	default:
@@ -237,22 +244,22 @@ TEE_Result cmd_prepare_key(uint32_t param_types, TEE_Param params[4])
 	res = TEE_AllocateOperation(&crypto_op, algo, mode, op_keysize);
 	CHECK(res, "TEE_AllocateOperation", return res;);
 
-	res = TEE_AllocateTransientObject(TEE_TYPE_AES, keysize, &hkey);
+	res = TEE_AllocateTransientObject(objectType, keysize, &hkey);
 	CHECK(res, "TEE_AllocateTransientObject", return res;);
 
 	attr.attributeID = TEE_ATTR_SECRET_VALUE;
-	attr.content.ref.buffer = aes_key;
+	attr.content.ref.buffer = cipher_key;
 	attr.content.ref.length = keysize / 8;
 
 	res = TEE_PopulateTransientObject(hkey, &attr, 1);
 	CHECK(res, "TEE_PopulateTransientObject", return res;);
 
 	if (algo == TEE_ALG_AES_XTS) {
-		res = TEE_AllocateTransientObject(TEE_TYPE_AES, keysize,
+		res = TEE_AllocateTransientObject(objectType, keysize,
 						  &hkey2);
 		CHECK(res, "TEE_AllocateTransientObject", return res;);
 
-		attr.content.ref.buffer = aes_key2;
+		attr.content.ref.buffer = cipher_key2;
 
 		res = TEE_PopulateTransientObject(hkey2, &attr, 1);
 		CHECK(res, "TEE_PopulateTransientObject", return res;);
@@ -282,6 +289,180 @@ TEE_Result cmd_prepare_key(uint32_t param_types, TEE_Param params[4])
 		TEE_CipherInit(crypto_op, ivp, ivlen);
 		return TEE_SUCCESS;
 	}
+}
+
+static bool is_mac(uint32_t hash_algo)
+{
+	switch (hash_algo) {
+	case TEE_ALG_HMAC_SHA1:
+	case TEE_ALG_HMAC_SHA224:
+	case TEE_ALG_HMAC_SHA256:
+	case TEE_ALG_HMAC_SHA384:
+	case TEE_ALG_HMAC_SHA512:
+	case TEE_ALG_HMAC_SM3:
+		return true;
+	default:
+		return false;
+	}
+}
+
+TEE_Result cmd_hash_process(uint32_t param_types, TEE_Param params[4])
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	TEE_OperationInfo info = { };
+	int n = 0;
+	void *in = NULL;
+	void *out = NULL;
+	size_t insz = 0;
+	size_t outsz = 0;
+	uint32_t offset = 0;
+	uint32_t exp_param_types = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
+						   TEE_PARAM_TYPE_MEMREF_OUTPUT,
+						   TEE_PARAM_TYPE_VALUE_INPUT,
+						   TEE_PARAM_TYPE_NONE);
+
+	if (param_types != exp_param_types)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	offset = params[2].value.b;
+	in = (uint8_t *)params[0].memref.buffer + offset;
+	insz = params[0].memref.size - offset;
+	out = params[1].memref.buffer;
+	outsz = params[1].memref.size;
+	n = params[2].value.a;
+
+	TEE_GetOperationInfo(crypto_op, &info);
+
+	if (is_mac(info.algorithm)) {
+		while (n--) {
+			TEE_MACInit(crypto_op, NULL, 0);
+			res = TEE_MACComputeFinal(crypto_op, in, insz, out, &outsz);
+			CHECK(res, "TEE_MACComputeFinal", return res;);
+		}
+	} else {
+		while (n--) {
+			res = TEE_DigestDoFinal(crypto_op, in, insz, out, &outsz);
+			CHECK(res, "TEE_DigestDoFinal", return res;);
+		}
+	}
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result cmd_hash_prepare_op(uint32_t param_types, TEE_Param params[4])
+{
+	TEE_ObjectHandle hkey = TEE_HANDLE_NULL;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	TEE_Attribute attr = { };
+	uint32_t key_type = TEE_TYPE_HMAC_SHA1;
+	uint32_t mac_key_size = 512;
+	uint32_t max_key_size = 0;
+	uint32_t hash_algo = 0;
+	static uint8_t mac_key[] = {
+		0x00, 0x01, 0x02, 0x03,
+		0x04, 0x05, 0x06, 0x07,
+		0x08, 0x09, 0x0A, 0x0B,
+		0x0C, 0x0D, 0x0E, 0x0F,
+		0x10, 0x11, 0x12, 0x13,
+		0x14, 0x15, 0x16, 0x17,
+		0x18, 0x19, 0x1A, 0x1B,
+		0x1C, 0x1D, 0x1E, 0x1F,
+		0x20, 0x21, 0x22, 0x23,
+		0x24, 0x25, 0x26, 0x27,
+		0x28, 0x29, 0x2A, 0x2B,
+		0x2C, 0x2D, 0x2E, 0x2F,
+		0x30, 0x31, 0x32, 0x33,
+		0x34, 0x35, 0x36, 0x37,
+		0x38, 0x39, 0x3A, 0x3B,
+		0x3C, 0x3D, 0x3E, 0x3F
+	};
+	uint32_t exp_param_types = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+						   TEE_PARAM_TYPE_NONE,
+						   TEE_PARAM_TYPE_NONE,
+						   TEE_PARAM_TYPE_NONE);
+
+	if (param_types != exp_param_types)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	switch (params[0].value.a) {
+	case TA_SHA_SHA1:
+		hash_algo = TEE_ALG_SHA1;
+		break;
+	case TA_SHA_SHA224:
+		hash_algo = TEE_ALG_SHA224;
+		break;
+	case TA_SHA_SHA256:
+		hash_algo = TEE_ALG_SHA256;
+		break;
+	case TA_SHA_SHA384:
+		hash_algo = TEE_ALG_SHA384;
+		break;
+	case TA_SHA_SHA512:
+		hash_algo = TEE_ALG_SHA512;
+		break;
+	case TA_SM3:
+		hash_algo = TEE_ALG_SM3;
+		break;
+	case TA_HMAC_SHA1:
+		key_type = TEE_TYPE_HMAC_SHA1;
+		hash_algo = TEE_ALG_HMAC_SHA1;
+		max_key_size = 512;
+		break;
+	case TA_HMAC_SHA224:
+		key_type = TEE_TYPE_HMAC_SHA224;
+		hash_algo = TEE_ALG_HMAC_SHA224;
+		max_key_size = 512;
+		break;
+	case TA_HMAC_SHA256:
+		key_type = TEE_TYPE_HMAC_SHA256;
+		hash_algo = TEE_ALG_HMAC_SHA256;
+		max_key_size = 512;
+		break;
+	case TA_HMAC_SHA384:
+		key_type = TEE_TYPE_HMAC_SHA384;
+		hash_algo = TEE_ALG_HMAC_SHA384;
+		max_key_size = 1024;
+		break;
+	case TA_HMAC_SHA512:
+		key_type = TEE_TYPE_HMAC_SHA512;
+		hash_algo = TEE_ALG_HMAC_SHA512;
+		max_key_size = 1024;
+		break;
+	case TA_HMAC_SM3:
+		key_type = TEE_TYPE_HMAC_SM3;
+		hash_algo = TEE_ALG_HMAC_SM3;
+		max_key_size = 512;
+		break;
+	default:
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	if (crypto_op)
+		TEE_FreeOperation(crypto_op);
+
+	if (is_mac(hash_algo)) {
+		res = TEE_AllocateOperation(&crypto_op, hash_algo, TEE_MODE_MAC, max_key_size);
+		CHECK(res, "TEE_AllocateOperation", return res;);
+
+		res = TEE_AllocateTransientObject(key_type, max_key_size, &hkey);
+		CHECK(res, "TEE_AllocateTransientObject", return res;);
+
+		attr.attributeID = TEE_ATTR_SECRET_VALUE;
+		attr.content.ref.buffer = mac_key;
+		attr.content.ref.length = mac_key_size / 8;
+
+		res = TEE_PopulateTransientObject(hkey, &attr, 1);
+		CHECK(res, "TEE_PopulateTransientObject", return res;);
+
+		res = TEE_SetOperationKey(crypto_op, hkey);
+		CHECK(res, "TEE_SetOperationKey", return res;);
+
+		TEE_FreeTransientObject(hkey);
+	} else {
+		res = TEE_AllocateOperation(&crypto_op, hash_algo, TEE_MODE_DIGEST, 0);
+		CHECK(res, "TEE_AllocateOperation", return res;);
+	}
+	return TEE_SUCCESS;
 }
 
 void cmd_clean_res(void)

Android.mk
@@ -108,8 +108,7 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/host/xtest \
 		$(LOCAL_PATH)/ta/storage_benchmark/include \
 		$(LOCAL_PATH)/ta/concurrent/include \
 		$(LOCAL_PATH)/ta/concurrent_large/include \
-		$(LOCAL_PATH)/ta/hash_perf/include \
-		$(LOCAL_PATH)/ta/aes_perf/include \
+		$(LOCAL_PATH)/ta/crypto_perf/include \
 		$(LOCAL_PATH)/ta/socket/include \
 		$(LOCAL_PATH)/ta/sdp_basic/include \
 		$(LOCAL_PATH)/ta/tpm_log_test/include \

host/xtest/Makefile
@@ -120,8 +120,7 @@ CFLAGS += -I../../ta/sims_keepalive/include
 CFLAGS += -I../../ta/storage_benchmark/include
 CFLAGS += -I../../ta/concurrent/include
 CFLAGS += -I../../ta/concurrent_large/include
-CFLAGS += -I../../ta/hash_perf/include
-CFLAGS += -I../../ta/aes_perf/include
+CFLAGS += -I../../ta/crypto_perf/include
 CFLAGS += -I../../ta/socket/include
 CFLAGS += -I../../ta/sdp_basic/include
 CFLAGS += -I../../ta/tpm_log_test/include

host/xtest/aes_perf.c
@@ -15,7 +15,7 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <ta_aes_perf.h>
+#include <ta_crypto_perf.h>
 #include <tee_client_api.h>
 #include <tee_client_api_extensions.h>
 #include <time.h>
@@ -109,7 +109,7 @@ static void check_res(TEEC_Result res, const char *errmsg, uint32_t *orig)
 static void open_ta(void)
 {
 	TEEC_Result res = TEEC_ERROR_GENERIC;
-	TEEC_UUID uuid = TA_AES_PERF_UUID;
+	TEEC_UUID uuid = TA_CRYPTO_PERF_UUID;
 	uint32_t err_origin = 0;
 
 	res = TEEC_InitializeContext(NULL, &ctx);
@@ -354,7 +354,7 @@ static void prepare_key(int decrypt, int keysize, int mode)
 	TEEC_Result res = TEEC_ERROR_GENERIC;
 	uint32_t ret_origin = 0;
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
-	uint32_t cmd = TA_AES_PERF_CMD_PREPARE_KEY;
+	uint32_t cmd = TA_CRYPTO_PERF_CMD_CIPHER_PREPARE_KEY;
 
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_VALUE_INPUT,
 					 TEEC_NONE, TEEC_NONE);
@@ -432,8 +432,8 @@ void aes_perf_run_test(int mode, int keysize, int decrypt, size_t size, size_t u
 	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 	int n0 = n;
 	double sd = 0;
-	uint32_t cmd = is_sdp_test ? TA_AES_PERF_CMD_PROCESS_SDP :
-				     TA_AES_PERF_CMD_PROCESS;
+	uint32_t cmd = is_sdp_test ? TA_CRYPTO_PERF_CMD_CIPHER_PROCESS_SDP :
+				     TA_CRYPTO_PERF_CMD_CIPHER_PROCESS;
 
 	if (input_buffer == BUFFER_UNSPECIFIED)
 		input_buffer = BUFFER_SHM_ALLOCATED;

host/xtest/crypto_common.h
@@ -7,9 +7,7 @@
 #ifndef XTEST_CRYPTO_COMMON_H
 #define XTEST_CRYPTO_COMMON_H
 
-#include "ta_aes_perf.h"
-#include "ta_hash_perf.h"
-
+#include "ta_crypto_perf.h"
 
 #define AES_PERF_INPLACE 0
 

host/xtest/hash_perf.c
@@ -53,7 +53,7 @@ static void check_res(TEEC_Result res, const char *errmsg, uint32_t *orig)
 static void open_ta(void)
 {
 	TEEC_Result res = TEEC_ERROR_GENERIC;
-	TEEC_UUID uuid = TA_HASH_PERF_UUID;
+	TEEC_UUID uuid = TA_CRYPTO_PERF_UUID;
 	uint32_t err_origin = 0;
 
 	res = TEEC_InitializeContext(NULL, &ctx);
@@ -247,7 +247,7 @@ static uint64_t run_test_once(void *in, size_t size, int random_in,
 		read_random(in, size);
 
 	get_current_time(&t0);
-	res = TEEC_InvokeCommand(&sess, TA_HASH_PERF_CMD_PROCESS, op,
+	res = TEEC_InvokeCommand(&sess, TA_CRYPTO_PERF_CMD_HASH_PROCESS, op,
 				 &ret_origin);
 	check_res(res, "TEEC_InvokeCommand", &ret_origin);
 	get_current_time(&t1);
@@ -264,7 +264,7 @@ static void prepare_op(int algo)
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
 	op.params[0].value.a = algo;
-	res = TEEC_InvokeCommand(&sess, TA_HASH_PERF_CMD_PREPARE_OP, &op,
+	res = TEEC_InvokeCommand(&sess, TA_CRYPTO_PERF_CMD_HASH_PREPARE_OP, &op,
 				 &ret_origin);
 	check_res(res, "TEEC_InvokeCommand", &ret_origin);
 }

ta/CMakeLists.txt
@@ -4,16 +4,15 @@ add_library(${PROJECT_NAME} INTERFACE)
 
 target_include_directories(${PROJECT_NAME}
 	INTERFACE include
-	INTERFACE aes_perf/include
 	INTERFACE concurrent/include
 	INTERFACE concurrent_large/include
 	INTERFACE create_fail_test/include
 	INTERFACE crypt/include
+	INTERFACE crypto_perf/include
 	INTERFACE enc_fs/include
 	INTERFACE os_test/include
 	INTERFACE rpc_test/include
 	INTERFACE sdp_basic/include
-	INTERFACE hash_perf/include
 	INTERFACE sims/include
 	INTERFACE miss/include
 	INTERFACE sims_keepalive/include

ta/Makefile
@@ -28,8 +28,7 @@ TA_DIRS := create_fail_test \
 	   concurrent \
 	   concurrent_large \
 	   storage_benchmark \
-	   hash_perf \
-	   aes_perf \
+	   crypto_perf \
 	   socket \
 	   supp_plugin \
 	   large \

commit b8261fe0ee8010f646a98041459ec9e463cbc4a8

ta: crypto_perf: correct coding style issue in symm tests
Sort out some coding style issue that are better being fixed for
test TAs implementation consistency. No functional changes.

Fixes: e18381f54eb8 ("xtest: combine aes_perf and hash_perf TAs into crypto_perf")
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

ta/crypto_perf/ta_crypto_perf.c
@@ -46,9 +46,9 @@ static bool is_inbuf_a_secure_memref(TEE_Param *param)
 	 * the buffer is nonsecure.
 	 */
 	res = TEE_CheckMemoryAccessRights(TEE_MEMORY_ACCESS_ANY_OWNER |
-					 TEE_MEMORY_ACCESS_READ |
-					 TEE_MEMORY_ACCESS_SECURE,
-					 param->memref.buffer, 1);
+					  TEE_MEMORY_ACCESS_READ |
+					  TEE_MEMORY_ACCESS_SECURE,
+					  param->memref.buffer, 1);
 	return (res == TEE_SUCCESS);
 }
 
@@ -63,9 +63,9 @@ static bool is_outbuf_a_secure_memref(TEE_Param *param)
 	 * the buffer is nonsecure.
 	 */
 	res = TEE_CheckMemoryAccessRights(TEE_MEMORY_ACCESS_ANY_OWNER |
-					 TEE_MEMORY_ACCESS_WRITE |
-					 TEE_MEMORY_ACCESS_SECURE,
-					 param->memref.buffer, 1);
+					  TEE_MEMORY_ACCESS_WRITE |
+					  TEE_MEMORY_ACCESS_SECURE,
+					  param->memref.buffer, 1);
 	return (res == TEE_SUCCESS);
 }
 
@@ -87,8 +87,8 @@ static __maybe_unused TEE_Result flush_memref_buffer(TEE_Param *param __unused)
 #endif /* CFG_CACHE_API */
 
 TEE_Result cmd_cipher_process(uint32_t param_types,
-		       TEE_Param params[TEE_NUM_PARAMS],
-		       bool use_sdp)
+			      TEE_Param params[TEE_NUM_PARAMS],
+			      bool use_sdp)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
 	int n = 0;
@@ -155,7 +155,7 @@ TEE_Result cmd_cipher_process(uint32_t param_types,
 		for (i = 0; i < insz / unit; i++) {
 			res = do_update(crypto_op, in, unit, out, &outsz);
 			CHECK(res, "TEE_CipherUpdate/TEE_AEUpdate", return res;);
-			in  = (void *)((uintptr_t)in + unit);
+			in = (void *)((uintptr_t)in + unit);
 			out = (void *)((uintptr_t)out + unit);
 		}
 		if (insz % unit) {
@@ -185,22 +185,18 @@ TEE_Result cmd_cipher_prepare_key(uint32_t param_types, TEE_Param params[4])
 	uint32_t keysize = 0;
 	const uint8_t *ivp = NULL;
 	size_t ivlen = 0;
-	static uint8_t cipher_key[] = { 0x00, 0x01, 0x02, 0x03,
-				     0x04, 0x05, 0x06, 0x07,
-				     0x08, 0x09, 0x0A, 0x0B,
-				     0x0C, 0x0D, 0x0E, 0x0F,
-				     0x10, 0x11, 0x12, 0x13,
-				     0x14, 0x15, 0x16, 0x17,
-				     0x18, 0x19, 0x1A, 0x1B,
-				     0x1C, 0x1D, 0x1E, 0x1F };
-	static uint8_t cipher_key2[] = { 0x20, 0x21, 0x22, 0x23,
-				      0x24, 0x25, 0x26, 0x27,
-				      0x28, 0x29, 0x2A, 0x2B,
-				      0x2C, 0x2D, 0x2E, 0x2F,
-				      0x30, 0x31, 0x32, 0x33,
-				      0x34, 0x35, 0x36, 0x37,
-				      0x38, 0x39, 0x3A, 0x3B,
-				      0x3C, 0x3D, 0x3E, 0x3F };
+	static uint8_t cipher_key[] = {
+		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+		0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+		0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+		0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F
+	};
+	static uint8_t cipher_key2[] = {
+		0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
+		0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
+		0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+		0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F
+	};
 	uint32_t exp_param_types = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
 						   TEE_PARAM_TYPE_VALUE_INPUT,
 						   TEE_PARAM_TYPE_NONE,
@@ -260,8 +256,7 @@ TEE_Result cmd_cipher_prepare_key(uint32_t param_types, TEE_Param params[4])
 	CHECK(res, "TEE_PopulateTransientObject", return res;);
 
 	if (algo == TEE_ALG_AES_XTS) {
-		res = TEE_AllocateTransientObject(objectType, keysize,
-						  &hkey2);
+		res = TEE_AllocateTransientObject(objectType, keysize, &hkey2);
 		CHECK(res, "TEE_AllocateTransientObject", return res;);
 
 		attr.content.ref.buffer = cipher_key2;
@@ -364,22 +359,14 @@ TEE_Result cmd_hash_prepare_op(uint32_t param_types, TEE_Param params[4])
 	uint32_t max_key_size = 0;
 	uint32_t hash_algo = 0;
 	static uint8_t mac_key[] = {
-		0x00, 0x01, 0x02, 0x03,
-		0x04, 0x05, 0x06, 0x07,
-		0x08, 0x09, 0x0A, 0x0B,
-		0x0C, 0x0D, 0x0E, 0x0F,
-		0x10, 0x11, 0x12, 0x13,
-		0x14, 0x15, 0x16, 0x17,
-		0x18, 0x19, 0x1A, 0x1B,
-		0x1C, 0x1D, 0x1E, 0x1F,
-		0x20, 0x21, 0x22, 0x23,
-		0x24, 0x25, 0x26, 0x27,
-		0x28, 0x29, 0x2A, 0x2B,
-		0x2C, 0x2D, 0x2E, 0x2F,
-		0x30, 0x31, 0x32, 0x33,
-		0x34, 0x35, 0x36, 0x37,
-		0x38, 0x39, 0x3A, 0x3B,
-		0x3C, 0x3D, 0x3E, 0x3F
+		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+		0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+		0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+		0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+		0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
+		0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
+		0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+		0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F
 	};
 	uint32_t exp_param_types = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
 						   TEE_PARAM_TYPE_NONE,