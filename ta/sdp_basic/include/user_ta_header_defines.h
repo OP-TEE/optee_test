42195fd2814f47e24b653ed01ea91aa4e53b82ae
Etienne Carriere <etienne.carriere@st.com>
2025-10-06 14:17:42 +0200

    ta: tree-wide: remove deprecated TA_FLAG_USER_MODE, TA_FLAG_EXEC_DDR
    Remove use of TA_FLAG_USER_MODE and TA_FLAG_EXEC_DDR that are deprecated
    since OP-TEE release 3.1.0.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- ta/sdp_basic/include/user_ta_header_defines.h
+++ ta/sdp_basic/include/user_ta_header_defines.h
@@ -11,9 +11,8 @@
 
 #define TA_UUID			TA_SDP_BASIC_UUID
 
-#define TA_FLAGS                (TA_FLAG_USER_MODE | TA_FLAG_EXEC_DDR | \
-				TA_FLAG_SECURE_DATA_PATH | \
-				TA_FLAG_CACHE_MAINTENANCE)
+#define TA_FLAGS                (TA_FLAG_SECURE_DATA_PATH | \
+				 TA_FLAG_CACHE_MAINTENANCE)
 
 #define TA_STACK_SIZE		(2 * 1024)
 #define TA_DATA_SIZE		(32 * 1024)
--- ta/storage/include/user_ta_header_defines.h
+++ ta/storage/include/user_ta_header_defines.h
@@ -15,8 +15,7 @@
  * This is important to have TA_FLAG_SINGLE_INSTANCE && !TA_FLAG_MULTI_SESSION
  * as it is used by the ytest
  */
-#define TA_FLAGS		(TA_FLAG_USER_MODE | TA_FLAG_EXEC_DDR | \
-				TA_FLAG_MULTI_SESSION)
+#define TA_FLAGS		TA_FLAG_MULTI_SESSION
 #define TA_STACK_SIZE		(2 * 1024)
 #define TA_DATA_SIZE		(64 * 1024)
 
