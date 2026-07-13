57fb68ed20286349abaef4d8550a645777f53aa9
Andrii Shtompel <andrii_shtompel@epam.com>
2026-07-13 10:38:15 +0000

    xtest: regression 1039: add android support
    Add Android.mk file for ta/subkey1 and ta/subkey2
    in xtest regression 1039
    
    Signed-off-by: Andrii Shtompel <andrii_shtompel@epam.com>
    Acked-by: Jerome Forissier <jerome.forissier@arm.com>

---
+++ ta/subkey1/Android.mk
@@ -0,0 +1,4 @@
+LOCAL_PATH := $(call my-dir)
+
+local_module := 5c206987-16a3-59cc-ab0f-64b9cfc9e758.ta
+include $(BUILD_OPTEE_MK)
---
+++ ta/subkey2/Android.mk
@@ -0,0 +1,4 @@
+LOCAL_PATH := $(call my-dir)
+
+local_module := a720ccbb-51da-417d-b82e-e5445d474a7a.ta
+include $(BUILD_OPTEE_MK)
